0d2ae1343cf224b77f1539c5b528ce3bcdc0ff2d
Sam Huynh <samhwang2112.dev@gmail.com>
2021-04-30 09:11:56 +1000

    Wrap all check rep and thank test in describe

--- discord-bot/src/thanks/thankUser.test.ts
+++ discord-bot/src/thanks/thankUser.test.ts
@@ -3,121 +3,123 @@ import { thankUser } from './thankUser';
 
 const replyMock = jest.fn(() => {});
 
-it('should do nothing if message not contains keyword', async () => {
-  const mockMsg: any = {
-    content: 'bla bla',
-    reply: replyMock,
-  };
-  const mockBotId = '0';
-  const mockPrisma: any = {};
+describe('thankUser', () => {
+  it('should do nothing if message not contains keyword', async () => {
+    const mockMsg: any = {
+      content: 'bla bla',
+      reply: replyMock,
+    };
+    const mockBotId = '0';
+    const mockPrisma: any = {};
 
-  await thankUser(mockMsg, mockBotId, mockPrisma);
+    await thankUser(mockMsg, mockBotId, mockPrisma);
 
-  expect(replyMock.mock.calls.length).toBe(0);
-});
+    expect(replyMock.mock.calls.length).toBe(0);
+  });
 
-it('should do nothing if mentions more than one user', async () => {
-  const mockUsers = new Collection<string, User>();
-  mockUsers.set('0', { id: '0' } as any);
-  mockUsers.set('1', { id: '1' } as any);
+  it('should do nothing if mentions more than one user', async () => {
+    const mockUsers = new Collection<string, User>();
+    mockUsers.set('0', { id: '0' } as any);
+    mockUsers.set('1', { id: '1' } as any);
 
-  const mockMsg: any = {
-    content: 'thank',
-    reply: replyMock,
-    mentions: {
-      users: mockUsers,
-    },
-  };
-  const mockBotId = '0';
-  const mockPrisma: any = {};
+    const mockMsg: any = {
+      content: 'thank',
+      reply: replyMock,
+      mentions: {
+        users: mockUsers,
+      },
+    };
+    const mockBotId = '0';
+    const mockPrisma: any = {};
 
-  await thankUser(mockMsg, mockBotId, mockPrisma);
+    await thankUser(mockMsg, mockBotId, mockPrisma);
 
-  expect(replyMock.mock.calls.length).toBe(0);
-});
+    expect(replyMock.mock.calls.length).toBe(0);
+  });
 
-it('should do nothing if user mention himself', async () => {
-  const mockUsers = new Collection<string, User>();
-  mockUsers.set('0', { id: '0' } as any);
+  it('should do nothing if user mention himself', async () => {
+    const mockUsers = new Collection<string, User>();
+    mockUsers.set('0', { id: '0' } as any);
 
-  const mockMsg: any = {
-    content: 'thank',
-    reply: replyMock,
-    mentions: {
-      users: mockUsers,
-    },
-    author: {
-      id: '0',
-    },
-  };
+    const mockMsg: any = {
+      content: 'thank',
+      reply: replyMock,
+      mentions: {
+        users: mockUsers,
+      },
+      author: {
+        id: '0',
+      },
+    };
 
-  const mockBotId = '0';
-  const mockPrisma: any = {};
+    const mockBotId = '0';
+    const mockPrisma: any = {};
 
-  await thankUser(mockMsg, mockBotId, mockPrisma);
+    await thankUser(mockMsg, mockBotId, mockPrisma);
 
-  expect(replyMock.mock.calls.length).toBe(0);
-});
+    expect(replyMock.mock.calls.length).toBe(0);
+  });
 
-it('should do nothing if bot is mentioned', async () => {
-  const mockUsers = new Collection<string, User>();
-  mockUsers.set('0', { id: '0' } as any);
+  it('should do nothing if bot is mentioned', async () => {
+    const mockUsers = new Collection<string, User>();
+    mockUsers.set('0', { id: '0' } as any);
 
-  const mockMsg: any = {
-    content: 'thank',
-    reply: replyMock,
-    mentions: {
-      users: mockUsers,
-    },
-    author: {
-      id: '1',
-    },
-  };
+    const mockMsg: any = {
+      content: 'thank',
+      reply: replyMock,
+      mentions: {
+        users: mockUsers,
+      },
+      author: {
+        id: '1',
+      },
+    };
 
-  const mockBotId = '0';
-  const mockPrisma: any = {};
+    const mockBotId = '0';
+    const mockPrisma: any = {};
 
-  await thankUser(mockMsg, mockBotId, mockPrisma);
+    await thankUser(mockMsg, mockBotId, mockPrisma);
 
-  expect(replyMock.mock.calls.length).toBe(0);
-});
+    expect(replyMock.mock.calls.length).toBe(0);
+  });
 
-const findUniqueMock = jest.fn(() => ({ id: '0' }));
-const updateUserMock = jest.fn();
-const reputationCreateMock = jest.fn();
-const transactionMock = jest.fn(() => [{ id: 0 }]);
-it('should call reply', async () => {
-  const mockUsers = new Collection<string, User>();
-  mockUsers.set('0', { id: '0' } as any);
+  const findUniqueMock = jest.fn(() => ({ id: '0' }));
+  const updateUserMock = jest.fn();
+  const reputationCreateMock = jest.fn();
+  const transactionMock = jest.fn(() => [{ id: 0 }]);
+  it('should call reply', async () => {
+    const mockUsers = new Collection<string, User>();
+    mockUsers.set('0', { id: '0' } as any);
 
-  const mockMsg: any = {
-    content: 'thank',
-    reply: replyMock,
-    mentions: {
-      users: mockUsers,
-    },
-    author: {
-      id: '1',
-    },
-  };
+    const mockMsg: any = {
+      content: 'thank',
+      reply: replyMock,
+      mentions: {
+        users: mockUsers,
+      },
+      author: {
+        id: '1',
+      },
+    };
 
-  const mockBotId = '2';
-  const mockPrisma: any = {
-    user: {
-      findUnique: findUniqueMock,
-      update: updateUserMock,
-    },
-    reputationLog: {
-      create: reputationCreateMock,
-    },
-    $transaction: transactionMock,
-  };
+    const mockBotId = '2';
+    const mockPrisma: any = {
+      user: {
+        findUnique: findUniqueMock,
+        update: updateUserMock,
+      },
+      reputationLog: {
+        create: reputationCreateMock,
+      },
+      $transaction: transactionMock,
+    };
 
-  await thankUser(mockMsg, mockBotId, mockPrisma);
+    await thankUser(mockMsg, mockBotId, mockPrisma);
 
-  expect(findUniqueMock.mock.calls.length).toBe(1);
-  expect(updateUserMock.mock.calls.length).toBe(1);
-  expect(reputationCreateMock.mock.calls.length).toBe(1);
-  expect(transactionMock.mock.calls.length).toBe(1);
-  expect(replyMock.mock.calls.length).toBe(1);
+    expect(findUniqueMock.mock.calls.length).toBe(1);
+    expect(updateUserMock.mock.calls.length).toBe(1);
+    expect(reputationCreateMock.mock.calls.length).toBe(1);
+    expect(transactionMock.mock.calls.length).toBe(1);
+    expect(replyMock.mock.calls.length).toBe(1);
+  });
 });
