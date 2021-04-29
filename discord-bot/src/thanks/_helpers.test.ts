39d48b9bb6b9d82f2d88d4dc3e4f046405d9d089
Eric Do <eric.do@go1.com>
2021-04-30 00:53:58 +1000

    add tests

--- discord-bot/src/thanks/_helpers.test.ts
+++ discord-bot/src/thanks/_helpers.test.ts
@@ -1,26 +1,26 @@
-import { getOrCreateUser } from "./_helpers"
+import { getOrCreateUser } from './_helpers';
 
 describe('getOrCreateUser', () => {
   it('should return user if user is existed', async () => {
     const mockPrisma: any = {
       user: {
-        findUnique: () => ({ id: '1' })
-      }
-    }
+        findUnique: () => ({ id: '1' }),
+      },
+    };
 
-    const user = await getOrCreateUser(mockPrisma, '1')
-    expect(user.id).toBe('1')
-  })
+    const user = await getOrCreateUser(mockPrisma, '1');
+    expect(user.id).toBe('1');
+  });
 
   it('should return user even if user is not existed', async () => {
     const mockPrisma: any = {
       user: {
         findUnique: () => null,
-        create: () => ({ id: '1' })
-      }
-    }
+        create: () => ({ id: '1' }),
+      },
+    };
 
-    const user = await getOrCreateUser(mockPrisma, '1')
-    expect(user.id).toBe('1')
-  })
-})
+    const user = await getOrCreateUser(mockPrisma, '1');
+    expect(user.id).toBe('1');
+  });
+});
