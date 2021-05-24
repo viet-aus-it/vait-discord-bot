6dd9ebbacf8d602086a2ed347e79c9c096255b37
Sam Huynh <samhwang2112.dev@gmail.com>
2021-05-24 16:21:14 +1000

    Refactor Message Processor module
    - Reorder function declarations to match call order
    - Remove exporting of not publicly needed types/interfaces.
    - Move the interfaces closer to their related function calls
    - Add new types for CommandPromise and CommandPromises to replace the
    Promise<any> | undefined return type
    - Add type KeywordMatchCommands for Array of KeywordMatchCommand

--- discord-bot/src/utils/messageProcessor.ts
+++ discord-bot/src/utils/messageProcessor.ts
@@ -1,88 +1,20 @@
 import { Message } from 'discord.js';
 
-export interface PrefixedCommand {
-  matcher: string;
-  fn: (message: Message) => Promise<any>;
-}
+type CommandPromise = Promise<any> | undefined;
 
-export interface PrefixedCommands {
-  prefix: string;
-  commands: Array<PrefixedCommand>;
-}
+type CommandPromises = Array<CommandPromise>;
 
-export interface KeywordMatchCommand {
+interface KeywordMatchCommand {
   matchers: Array<string>;
   fn: (message: Message) => Promise<any>;
 }
-export interface EmojiMatchCommand {
-  matcher: string;
-  fn: (message: Message) => Promise<any>;
-}
 
-export interface LinkMatchCommand {
-  fn: (message: Message) => Promise<any>;
-}
-export interface CommandConfig {
-  prefixedCommands: PrefixedCommands;
-  keywordMatchCommands: Array<KeywordMatchCommand>;
-  emojiMatchCommand: EmojiMatchCommand;
-  linkMatchCommand: LinkMatchCommand;
-}
-
-export const processMessage = async (
-  message: Message,
-  config: CommandConfig
-) => {
-  const keywordPromises = processKeywordMatch(
-    message,
-    config.keywordMatchCommands
-  );
-  const prefixPromises = processPrefixedMatch(message, config.prefixedCommands);
-  const emojiPromises =
-    prefixPromises.filter((p) => p !== undefined).length > 0
-      ? undefined
-      : processEmojiMatch(message, config.emojiMatchCommand); // if match any prefix commands, don't process emoji
-  const linkPromises =
-    prefixPromises.filter((p) => p !== undefined).length > 0
-      ? undefined
-      : processLinkMatch(message, config.linkMatchCommand); // if match any prefix commands, don't process link
-  const promises = [
-    ...keywordPromises,
-    ...prefixPromises,
-    emojiPromises,
-    linkPromises,
-  ].filter((p) => p !== undefined);
-  await Promise.all(promises).catch(console.error);
-};
-
-const processEmojiMatch = (
-  message: Message,
-  config: EmojiMatchCommand
-): Promise<any> | undefined => {
-  const hasEmoji = message.content.match(config.matcher);
-  if (!hasEmoji) {
-    return;
-  }
-  return config.fn(message);
-};
-
-const processLinkMatch = (
-  message: Message,
-  config: LinkMatchCommand
-): Promise<any> | undefined => {
-  const hasDiscordLink = message.content.match(
-    /https:\/\/discord\.com\/channels\/\d+\/\d+\/\d+/gim
-  );
-  if (!hasDiscordLink) {
-    return;
-  }
-  return config.fn(message);
-};
+type KeywordMatchCommands = Array<KeywordMatchCommand>;
 
 const processKeywordMatch = (
   message: Message,
-  config: Array<KeywordMatchCommand>
-): Array<Promise<any> | undefined> => {
+  config: KeywordMatchCommands
+): CommandPromises => {
   return config.map((conf) => {
     const hasKeyword = conf.matchers.some((keyword) =>
       message.content.toLowerCase().includes(keyword)
@@ -96,10 +28,20 @@ const processKeywordMatch = (
   });
 };
 
+interface PrefixedCommand {
+  matcher: string;
+  fn: (message: Message) => Promise<any>;
+}
+
+interface PrefixedCommands {
+  prefix: string;
+  commands: Array<PrefixedCommand>;
+}
+
 const processPrefixedMatch = (
   message: Message,
   config: PrefixedCommands
-): Array<Promise<any> | undefined> => {
+): CommandPromises => {
   return config.commands.map((conf) => {
     const prefixCommand = `${config.prefix}${conf.matcher}`;
     const hasMatchingPrefix = message.content.startsWith(prefixCommand);
@@ -110,4 +52,82 @@ const processPrefixedMatch = (
 
     return conf.fn(message);
   });
+};
+
+interface EmojiMatchCommand {
+  matcher: string;
+  fn: (message: Message) => Promise<any>;
+}
+
+const processEmojiMatch = (
+  message: Message,
+  config: EmojiMatchCommand
+): CommandPromise => {
+  const hasEmoji = message.content.match(config.matcher);
+  if (!hasEmoji) {
+    return;
+  }
+
+  return config.fn(message);
+};
+
+interface LinkMatchCommand {
+  fn: (message: Message) => Promise<any>;
+}
+
+const processLinkMatch = (
+  message: Message,
+  config: LinkMatchCommand
+): CommandPromise => {
+  const hasDiscordLink = message.content.match(
+    /https:\/\/discord\.com\/channels\/\d+\/\d+\/\d+/gim
+  );
+  if (!hasDiscordLink) {
+    return;
+  }
+
+  return config.fn(message);
+};
+
+const removeUndefinedPromises = (promises: CommandPromises) =>
+  promises.filter((p) => p !== undefined);
+
+export interface CommandConfig {
+  prefixedCommands: PrefixedCommands;
+  keywordMatchCommands: KeywordMatchCommands;
+  emojiMatchCommand: EmojiMatchCommand;
+  linkMatchCommand: LinkMatchCommand;
+}
+
+export const processMessage = async (
+  message: Message,
+  config: CommandConfig
+) => {
+  const keywordPromises = processKeywordMatch(
+    message,
+    config.keywordMatchCommands
+  );
+  const prefixPromises = processPrefixedMatch(message, config.prefixedCommands);
+
+  // If message already has a prefix command, don't process these.
+  const hasPrefixPromises = removeUndefinedPromises(prefixPromises).length > 0;
+  const emojiPromises = hasPrefixPromises
+    ? undefined
+    : processEmojiMatch(message, config.emojiMatchCommand);
+  const linkPromises = hasPrefixPromises
+    ? undefined
+    : processLinkMatch(message, config.linkMatchCommand);
+
+  const promises = removeUndefinedPromises([
+    ...keywordPromises,
+    ...prefixPromises,
+    emojiPromises,
+    linkPromises,
+  ]);
+
+  try {
+    await Promise.all(promises);
+  } catch (error) {
+    console.error('ERROR PROCESSING MESSAGE', error);
+  }
 };
