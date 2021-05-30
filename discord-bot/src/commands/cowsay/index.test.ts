3dc84d46dc7f24e67fc124fcc18b5535657d4b31
ThanhNguyen <nnct2000@gmail.com>
2021-05-30 19:35:24 +1000

    Add Test cases for Cowsay function

---
+++ discord-bot/src/commands/cowsay/index.test.ts
@@ -0,0 +1,49 @@
+import cowsay from '.';
+
+const replyMock = jest.fn(() => {});
+
+describe('cowsay test', () => {
+  it('It should reply for any text from non-bot user', async () => {
+    const mockMsg: any = {
+      content: '-cowsay lorem ipsum',
+      channel: { send: replyMock },
+      author: { bot: false },
+    };
+
+    await cowsay(mockMsg);
+    expect(replyMock).toHaveBeenCalledTimes(1);
+  });
+
+  it('It should eliminate BackTicks of the message (discord codeblock)', async () => {
+    const mockMsg: any = {
+      content: '-cowsay ```lorem ipsum```',
+      channel: { send: replyMock },
+      author: { bot: false },
+    };
+
+    await cowsay(mockMsg);
+    expect(replyMock).toBe('lorem ipsum');
+  });
+
+  it('Should not reply if there is no content and reference', async () => {
+    const mockMsg: any = {
+      content: '-cowsay ',
+      channel: { send: replyMock },
+      author: { bot: false },
+    };
+
+    await cowsay(mockMsg);
+    expect(replyMock).not.toHaveBeenCalled();
+  });
+
+  it('Should not reply if the sender is a bot', async () => {
+    const mockMsg: any = {
+      content: '-cowsay I am a bot',
+      channel: { send: replyMock },
+      author: { bot: true },
+    };
+
+    await cowsay(mockMsg);
+    expect(replyMock).not.toHaveBeenCalled();
+  });
+});
