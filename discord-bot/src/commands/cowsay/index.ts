27fd5a7456ce1e7a49c70e4421b24a3b2457d83d
ThanhNguyen <nnct2000@gmail.com>
2021-06-03 20:31:37 +1000

    Rewrite Cowsay to handle previous message

--- discord-bot/src/commands/cowsay/index.ts
+++ discord-bot/src/commands/cowsay/index.ts
@@ -1,13 +1,19 @@
 import { Message, TextChannel } from 'discord.js';
 import * as Cowsay from 'cowsay';
-import { fetchMessageById } from '../../utils/messageFetcher';
+import {
+  fetchLastMessageBeforeId,
+  fetchMessageById,
+} from '../../utils/messageFetcher';
 
 const isBlank = (content: string) => content.trim() === '';
 
+const replaceAll = (string: string, from: string, to: string) =>
+  string.split(from).join(to);
+
 // Remove backtick in message in case of nesting cowsay
 const removeBacktick = (message: string) => {
-  if (message.indexOf('```') !== -1) {
-    return message.slice(3, -3);
+  if (message.indexOf('`') !== -1) {
+    return replaceAll(message, '`', '');
   }
   return message;
 };
@@ -19,24 +25,27 @@ const generateCowsayText = (message: string) => {
   return Cowsay.say(config);
 };
 
-const cowsay = async ({ content, reference, channel, author }: Message) => {
+const cowsay = async ({ content, reference, channel, id, author }: Message) => {
   // Return if sender is bot
   if (author.bot) return;
 
-  let chatContent =
+  let chatContent = removeBacktick(
     content.trimEnd().indexOf(' ') !== -1
       ? content.slice(content.trimEnd().indexOf(' ')).trimStart()
-      : '';
+      : ''
+  );
 
-  // If there is no chat content, then assign any reference as chat content
+  // If there is no chat content...
   if (isBlank(chatContent)) {
     if (reference && reference.channelID !== null) {
+      // And there is a reference to another message, fetch that message
       chatContent = await fetchMessageById(
         channel as TextChannel,
         reference.messageID as string
       );
     } else {
-      return;
+      // Or just fetch the latest message
+      chatContent = await fetchLastMessageBeforeId(channel as TextChannel, id);
     }
   }
 
