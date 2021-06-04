ad27b60bf847248b8ae22818263c61d24d20c9ba
Thanh Nguyen <nnct2000@gmail.com>
2021-06-04 17:30:44 +1000

    Enforce long text to be wrapped

--- discord-bot/src/commands/cowsay/index.ts
+++ discord-bot/src/commands/cowsay/index.ts
@@ -1,9 +1,12 @@
 import { Message, TextChannel } from 'discord.js';
-import * as Cowsay from 'cowsay';
+import { say } from 'cowsay';
 import {
   fetchLastMessageBeforeId,
   fetchMessageById,
 } from '../../utils/messageFetcher';
+
+// Only 35 characters per line due to limitation in phone screen width
+const WRAP_TEXT_LIMIT = 35;
 
 const isBlank = (content: string) => content.trim() === '';
 
@@ -11,18 +14,57 @@ const replaceAll = (string: string, from: string, to: string) =>
   string.split(from).join(to);
 
 // Remove backtick in message in case of nesting cowsay
-const removeBacktick = (message: string) => {
+export const removeBacktick = (message: string) => {
   if (message.indexOf('`') !== -1) {
     return replaceAll(message, '`', '');
   }
   return message;
 };
 
+// Wrap text in multiple lines in case of long text input
+const wrapText = (input: string, width: number) => {
+  const resultArray = [];
+  const words = input.split(' ');
+  let line = '';
+
+  // Check each words
+  words.forEach((word) => {
+    if ((line + word).length <= width) {
+      // Add a word if line lenght is within limit
+      line += (line ? ' ' : '') + word;
+    } else {
+      // Register and skip to next line if too long
+      resultArray.push(line);
+      line = word;
+    }
+  });
+
+  // Add any leftover line to next line.
+  if (line) {
+    resultArray.push(line);
+  }
+
+  return resultArray.join('\n');
+};
+
 const generateCowsayText = (message: string) => {
   const config = {
     text: message,
   };
-  return Cowsay.say(config);
+  return say(config);
+};
+
+// Send Cowsay text
+const sendCowsay = async (string: string, channel: any) => {
+  let chatContent = removeBacktick(string);
+  chatContent = wrapText(chatContent, WRAP_TEXT_LIMIT);
+  const reply = `\`\`\`${generateCowsayText(chatContent)}\`\`\``;
+
+  try {
+    await channel.send(reply);
+  } catch (error) {
+    console.error('CANNOT SEND MESSAGE', error);
+  }
 };
 
 const cowsay = async ({ content, reference, channel, id, author }: Message) => {
@@ -35,28 +77,30 @@ const cowsay = async ({ content, reference, channel, id, author }: Message) => {
       : ''
   );
 
+  // If cowsay is called with chat content
+  if (!isBlank(chatContent)) {
+    sendCowsay(chatContent, channel);
+    return;
+  }
+
   // If there is no chat content...
+  if (reference && reference.channelID !== null) {
+    // And there is a reference to another message, fetch that message
+    chatContent = await fetchMessageById(
+      channel as TextChannel,
+      reference.messageID as string
+    );
+  } else {
+    // Or just fetch the latest message
+    chatContent = await fetchLastMessageBeforeId(channel as TextChannel, id);
+  }
+
+  // if the content stil blank at this point, exit
   if (isBlank(chatContent)) {
-    if (reference && reference.channelID !== null) {
-      // And there is a reference to another message, fetch that message
-      chatContent = await fetchMessageById(
-        channel as TextChannel,
-        reference.messageID as string
-      );
-    } else {
-      // Or just fetch the latest message
-      chatContent = await fetchLastMessageBeforeId(channel as TextChannel, id);
-    }
+    return;
   }
 
-  chatContent = removeBacktick(chatContent);
-  const reply = `\`\`\`${generateCowsayText(chatContent)}\`\`\``;
-
-  try {
-    await channel.send(reply);
-  } catch (error) {
-    console.error('CANNOT SEND MESSAGE', error);
-  }
+  sendCowsay(chatContent, channel);
 };
 
 export default cowsay;
