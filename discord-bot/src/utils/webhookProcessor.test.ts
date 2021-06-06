40e45cc0ef103fd695b717964302d6e09695389e
Sam Huynh <samhwang2112.dev@gmail.com>
2021-06-06 20:47:58 +1000

    Add fetch or create webhook test

--- discord-bot/src/utils/webhookProcessor.test.ts
+++ discord-bot/src/utils/webhookProcessor.test.ts
@@ -1,5 +1,9 @@
 import { Collection, Webhook } from 'discord.js';
-import { fetchWebhook, createWebhook } from './webhookProcessor';
+import {
+  fetchWebhook,
+  createWebhook,
+  fetchOrCreateWebhook,
+} from './webhookProcessor';
 
 describe('Test webhook processor', () => {
   it('should return an existing webhook', async () => {
@@ -34,5 +38,41 @@ describe('Test webhook processor', () => {
     const result = await createWebhook(mockedChannel, 'VAIT-Hook');
     expect(mockCreate).toHaveBeenCalled();
     expect(result).toEqual(mockedWebhook);
+  });
+
+  it('should create a new webhook if cannot be fetched', async () => {
+    const webhooks = new Collection<string, Webhook>();
+    const mockedFetch = jest.fn(async () => webhooks);
+    const mockedWebhook: any = {
+      name: 'VAIT-Hook',
+      channelID: 123456,
+    };
+    const mockCreate = jest.fn(async () => mockedWebhook);
+    const mockedChannel: any = {
+      id: 123456,
+      fetchWebhooks: mockedFetch,
+      createWebhook: mockCreate,
+    };
+
+    const result = await fetchOrCreateWebhook(mockedChannel, 'VAIT-Hook');
+    expect(mockedFetch).toHaveBeenCalled();
+    expect(mockCreate).toHaveBeenCalled();
+    expect(result).toEqual(mockedWebhook);
+  });
+
+  it('Should return undefined if a webhook cannot be fetched and/or created', async () => {
+    const webhooks = new Collection<string, Webhook>();
+    const mockedFetch = jest.fn(async () => webhooks);
+    const mockCreate = jest.fn(async () => undefined);
+    const mockedChannel: any = {
+      id: 123456,
+      fetchWebhooks: mockedFetch,
+      createWebhook: mockCreate,
+    };
+
+    const result = await fetchOrCreateWebhook(mockedChannel, 'VAIT-Hook');
+    expect(mockedFetch).toHaveBeenCalled();
+    expect(mockCreate).toHaveBeenCalled();
+    expect(result).toEqual(undefined);
   });
 });
