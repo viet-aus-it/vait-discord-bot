caa25b3679ca4624d486d1ae942cd1c1181a0480
Thanh Nguyen <nnct2000@gmail.com>
2021-06-08 03:16:21 +1000

    Add new module insult

---
+++ discord-bot/src/commands/insult/index.ts
@@ -0,0 +1,13 @@
+import { Message, TextChannel } from 'discord.js';
+
+const FETCH_URL = 'http://www.pangloss.com/seidel/Shaker/';
+
+const insult = async ({
+    content,
+    channel,
+    id,
+    reference,
+    author,
+}: Message) => {
+    
+}
