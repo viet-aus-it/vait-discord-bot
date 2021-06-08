6ab394d907c4b465325286e3b90d84672596369b
ThanhNguyen <nnct2000@gmail.com>
2021-06-09 01:30:52 +1000

    Create insult function
    - Insult people
    - Insult message author (quote it)
    - Chí Phèo mode: chắc nó chừa mình ra!

--- discord-bot/src/commands/insult/index.ts
+++ discord-bot/src/commands/insult/index.ts
@@ -1,13 +1,51 @@
 import { Message, TextChannel } from 'discord.js';
+import { fetchMessageObjectById } from '../../utils/messageFetcher';
+import { randomInsultGenerator } from './insultGenerator';
 
-const FETCH_URL = 'http://www.pangloss.com/seidel/Shaker/';
+const isBlank = (content: string) => content.trim() === '';
 
-const insult = async ({
-    content,
-    channel,
-    id,
-    reference,
-    author,
-}: Message) => {
-    
-}
+const sendInsult = async (insult: string, channel: TextChannel) => {
+  try {
+    await channel.send(insult);
+  } catch (error) {
+    console.error('CANNOT SEND MESSAGE', error);
+  }
+};
+
+const insult = async ({ content, reference, channel, author }: Message) => {
+  if (author.bot) return;
+
+  const textChannel = channel as TextChannel;
+
+  const firstSpaceChar = content.trimEnd().indexOf(' ');
+
+  const insultText = randomInsultGenerator() as string;
+
+  let chatContent =
+    firstSpaceChar !== -1 ? content.slice(firstSpaceChar).trimStart() : '';
+
+  // If there is chat content
+  if (!isBlank(chatContent)) {
+    const replyText = `${chatContent}, ${insultText.toLowerCase()}`;
+    sendInsult(replyText, textChannel);
+    return;
+  }
+
+  // If there is a reference to a msg
+  if (reference && reference.messageID !== null) {
+    // Then insult the author of the refered message
+    const referredMsg = await fetchMessageObjectById(
+      channel as TextChannel,
+      reference.messageID
+    );
+    const referedAuthorId = referredMsg.author.id;
+    chatContent = `<@!${referedAuthorId}>, ${insultText.toLowerCase()}`;
+    sendInsult(chatContent, textChannel);
+    return;
+  }
+
+  // If there is no content or reference
+  sendInsult(insultText, textChannel);
+};
+
+export default insult;
