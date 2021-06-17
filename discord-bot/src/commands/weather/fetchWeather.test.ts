7420f1efd611f17636b22795048dc17e1e46ca6a
Thanh Nguyen <nnct2000@gmail.com>
2021-06-17 14:56:58 +1000

    Add test cases for fetch weather

---
+++ discord-bot/src/commands/weather/fetchWeather.test.ts
@@ -0,0 +1,30 @@
+import faker from 'faker';
+import fetch from 'node-fetch';
+import fetchWeather from './fetchWeather';
+
+jest.mock('node-fetch');
+const mockFetch = fetch as jest.MockedFunction<typeof fetch>;
+
+describe('Fetch weather tests', () => {
+  it('Should return undefined if cannot get weather', async () => {
+    const mockedWeather: any = undefined;
+    mockFetch.mockImplementationOnce(async () => mockedWeather);
+
+    const output = await fetchWeather('Melbourne');
+    expect(output).toEqual(undefined);
+  });
+
+  it('Should return weather when available', async () => {
+    const fakeWeather: any = faker.lorem.words(10);
+    // const sampleWeather = { weather: fakeWeather };
+
+    const mockedWeather: any = { text: async () => fakeWeather };
+
+    mockFetch.mockImplementationOnce(async () => mockedWeather);
+    const output = await fetchWeather('');
+
+    expect(output).toEqual({
+      weather: fakeWeather,
+    });
+  });
+});
