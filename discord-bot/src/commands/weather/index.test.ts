ec331a5c9944cb1a2d290a1da63e794f5804aea3
Thanh Nguyen <nnct2000@gmail.com>
2021-06-17 14:55:59 +1000

    Add Test cases for Weather function

---
+++ discord-bot/src/commands/weather/index.test.ts
@@ -0,0 +1,61 @@
+import faker from 'faker';
+import weather from '.';
+import fetchWeather from './fetchWeather';
+
+jest.mock('./fetchWeather');
+const mockFetch = fetchWeather as jest.MockedFunction<typeof fetchWeather>;
+
+const replyMock = jest.fn(() => {});
+
+describe('Weather test', () => {
+  it('Should not reply bot', async () => {
+    const mockMsg: any = {
+      content: `-weather`,
+      channel: { send: replyMock },
+      author: { bot: true },
+    };
+
+    await weather(mockMsg);
+    expect(replyMock).not.toHaveBeenCalled();
+  });
+
+  it('Should reply command from non-bot user', async () => {
+    const mockMsg: any = {
+      content: `-weather Hanoi`,
+      channel: { send: replyMock },
+      author: { bot: false },
+    };
+
+    const fakeWeather = faker.lorem.words(10);
+    mockFetch.mockImplementationOnce(async () => ({ weather: fakeWeather }));
+
+    await weather(mockMsg);
+    expect(replyMock).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should run even if there is no content', async () => {
+    const mockMsg: any = {
+      content: `-weather`,
+      channel: { send: replyMock },
+      author: { bot: false },
+    };
+
+    const fakeWeather = faker.lorem.words(10);
+    mockFetch.mockImplementationOnce(async () => ({ weather: fakeWeather }));
+
+    await weather(mockMsg);
+    expect(replyMock).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should return if there is no weather downloaded', async () => {
+    const mockMsg: any = {
+      content: `-weather`,
+      channel: { send: replyMock },
+      author: { bot: false },
+    };
+
+    mockFetch.mockImplementationOnce(async () => undefined);
+    await weather(mockMsg);
+    expect(replyMock).not.toHaveBeenCalled();
+  });
+});
