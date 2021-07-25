3b25faa30a55921fab5227b85fba985f07997548
Sam Huynh <samhwang2112.dev@gmail.com>
2021-07-25 16:51:56 +1000

    Add test cases for rep commands
    Also refactor older test cases to use the helper modules instead of calling on Prisma directly

--- discord-bot/src/commands/reputation/giveReputation.test.ts
+++ discord-bot/src/commands/reputation/giveReputation.test.ts
@@ -1,18 +1,36 @@
 import { Collection, User } from 'discord.js';
 import { mocked } from 'ts-jest/utils';
 import { giveReputation } from './giveReputation';
-import { getPrismaClient } from '../../clients/prisma';
+import { getOrCreateUser, updateRep } from './_helpers';
 
-jest.mock('../../clients/prisma');
-const mockGetPrismaClient = mocked(getPrismaClient);
+jest.mock('./_helpers');
+const mockCreateUpdateUser = mocked(getOrCreateUser);
+const mockUpdateRep = mocked(updateRep);
 
 const replyMock = jest.fn(() => {});
 
 describe('giveRep', () => {
-  const findUniqueMock = jest.fn(() => ({ id: '0' }));
-  const updateUserMock = jest.fn();
-  const reputationCreateMock = jest.fn();
-  const transactionMock = jest.fn(() => [{ id: 0 }]);
+  it('should do nothing if bot is saying the keywords', async () => {
+    const mockUsers = new Collection<string, User>();
+    mockUsers.set('0', { id: '2' } as User);
+
+    const mockMsg: any = {
+      content: 'thank',
+      reply: replyMock,
+      mentions: {
+        users: mockUsers,
+      },
+      author: {
+        id: '0',
+        bot: true,
+      },
+    };
+
+    await giveReputation(mockMsg);
+    expect(mockCreateUpdateUser).not.toHaveBeenCalled();
+    expect(mockUpdateRep).not.toHaveBeenCalled();
+    expect(replyMock).not.toHaveBeenCalled();
+  });
 
   it('should do nothing if mentions more than one user', async () => {
     const mockUsers = new Collection<string, User>();
@@ -30,36 +48,9 @@ describe('giveRep', () => {
       },
     };
 
-    const mockPrismaClient: any = {};
-    mockGetPrismaClient.mockReturnValue(mockPrismaClient);
-
     await giveReputation(mockMsg);
 
     expect(replyMock).not.toHaveBeenCalled();
-  });
-
-  it('should send reject message if user mention himself', async () => {
-    const mockUsers = new Collection<string, User>();
-    mockUsers.set('0', { id: '0' } as User);
-
-    const mockMsg: any = {
-      content: 'thank',
-      reply: replyMock,
-      mentions: {
-        users: mockUsers,
-      },
-      author: {
-        id: '0',
-        bot: false,
-      },
-    };
-
-    const mockPrismaClient: any = {};
-    mockGetPrismaClient.mockReturnValue(mockPrismaClient);
-
-    await giveReputation(mockMsg);
-
-    expect(replyMock).toHaveBeenCalled();
   });
 
   it('should do nothing if user mention no one', async () => {
@@ -81,23 +72,10 @@ describe('giveRep', () => {
       },
     };
 
-    const mockPrismaClient: any = {
-      user: {
-        findUnique: findUniqueMock,
-        update: updateUserMock,
-      },
-      reputationLog: {
-        create: reputationCreateMock,
-      },
-      $transaction: transactionMock,
-    };
-    mockGetPrismaClient.mockReturnValue(mockPrismaClient);
-
     await giveReputation(mockMsg);
-    expect(findUniqueMock).not.toHaveBeenCalled();
+    expect(mockCreateUpdateUser).not.toHaveBeenCalled();
+    expect(mockUpdateRep).not.toHaveBeenCalled();
     expect(replyMock).not.toHaveBeenCalled();
-    expect(reputationCreateMock).not.toHaveBeenCalled();
-    expect(transactionMock).not.toHaveBeenCalled();
   });
 
   it('should do nothing if bot is mentioned', async () => {
@@ -116,28 +94,15 @@ describe('giveRep', () => {
       },
     };
 
-    const mockPrismaClient: any = {
-      user: {
-        findUnique: findUniqueMock,
-        update: updateUserMock,
-      },
-      reputationLog: {
-        create: reputationCreateMock,
-      },
-      $transaction: transactionMock,
-    };
-    mockGetPrismaClient.mockReturnValue(mockPrismaClient);
-
     await giveReputation(mockMsg);
-    expect(findUniqueMock).not.toHaveBeenCalled();
+    expect(mockCreateUpdateUser).not.toHaveBeenCalled();
+    expect(mockUpdateRep).not.toHaveBeenCalled();
     expect(replyMock).not.toHaveBeenCalled();
-    expect(reputationCreateMock).not.toHaveBeenCalled();
-    expect(transactionMock).not.toHaveBeenCalled();
   });
 
-  it('should do nothing if bot is saying the keywords', async () => {
+  it('should send reject message if user mention himself', async () => {
     const mockUsers = new Collection<string, User>();
-    mockUsers.set('0', { id: '2' } as User);
+    mockUsers.set('0', { id: '0' } as User);
 
     const mockMsg: any = {
       content: 'thank',
@@ -147,32 +112,20 @@ describe('giveRep', () => {
       },
       author: {
         id: '0',
-        bot: true,
+        bot: false,
       },
     };
-
-    const mockPrismaClient: any = {
-      user: {
-        findUnique: findUniqueMock,
-        update: updateUserMock,
-      },
-      reputationLog: {
-        create: reputationCreateMock,
-      },
-      $transaction: transactionMock,
-    };
-    mockGetPrismaClient.mockReturnValue(mockPrismaClient);
 
     await giveReputation(mockMsg);
-    expect(findUniqueMock).not.toHaveBeenCalled();
-    expect(replyMock).not.toHaveBeenCalled();
-    expect(reputationCreateMock).not.toHaveBeenCalled();
-    expect(transactionMock).not.toHaveBeenCalled();
+
+    expect(replyMock).toHaveBeenCalled();
   });
 
   it('should call reply', async () => {
     const mockUsers = new Collection<string, User>();
     mockUsers.set('0', { id: '0' } as User);
+    mockCreateUpdateUser.mockResolvedValueOnce({ id: '0', reputation: 0 });
+    mockUpdateRep.mockResolvedValueOnce({ id: '0', reputation: 1 });
 
     const mockMsg: any = {
       content: 'thank',
@@ -185,25 +138,10 @@ describe('giveRep', () => {
         id: '1',
       },
     };
-
-    const mockPrismaClient: any = {
-      user: {
-        findUnique: findUniqueMock,
-        update: updateUserMock,
-      },
-      reputationLog: {
-        create: reputationCreateMock,
-      },
-      $transaction: transactionMock,
-    };
-    mockGetPrismaClient.mockReturnValue(mockPrismaClient);
-
     await giveReputation(mockMsg);
 
-    expect(findUniqueMock).toHaveBeenCalledTimes(1);
-    expect(updateUserMock).toHaveBeenCalledTimes(1);
-    expect(reputationCreateMock).toHaveBeenCalledTimes(1);
-    expect(transactionMock).toHaveBeenCalledTimes(1);
+    expect(mockCreateUpdateUser).toHaveBeenCalledTimes(1);
+    expect(mockUpdateRep).toHaveBeenCalledTimes(1);
     expect(replyMock).toHaveBeenCalledTimes(1);
   });
 });
