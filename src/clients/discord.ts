876ac7aca78f3ebf8671c94861b83ec54fa09750
Sam Huynh <samhwang2112.dev@gmail.com>
2021-12-28 00:33:07 +1100

    Add Guild intent to discord client config

--- src/clients/discord.ts
+++ src/clients/discord.ts
@@ -1,4 +1,4 @@
-import { Client } from 'discord.js';
+import { Client, Intents } from 'discord.js';
 
 export type ClientOptions = {
   token?: string;
@@ -7,7 +7,9 @@ export const getDiscordClient = (options: ClientOptions): Promise<Client> => {
   if (!options.token) throw new Error('please setup bot token');
 
   return new Promise((resolve, reject) => {
-    const client = new Client();
+    const client = new Client({
+      intents: [Intents.FLAGS.GUILDS],
+    });
     client
       .on('ready', () => resolve(client))
       .on('error', reject)
