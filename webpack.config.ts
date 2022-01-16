9da9bfedc8848639cf65129f212ee8c97812a86d
Sam Huynh <samhwang2112.dev@gmail.com>
2022-01-16 15:37:58 +1100

    Hot Fix: Copy prisma built assets into the built server folder

--- webpack.config.ts
+++ webpack.config.ts
@@ -59,7 +59,7 @@ const config: Configuration = {
             'client',
             'libquery_engine-debian-openssl-1.1.x.so.node'
           ),
-          to: outputPath,
+          to: path.resolve(outputPath, 'server'),
         },
         {
           from: path.resolve(
@@ -69,7 +69,7 @@ const config: Configuration = {
             'client',
             'schema.prisma'
           ),
-          to: outputPath,
+          to: path.resolve(outputPath, 'server'),
         },
         {
           from: path.resolve(
