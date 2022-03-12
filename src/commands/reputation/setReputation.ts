4844461f341fae7f3276dd9c500a61fedc30dc4d
Sam Huynh <samhwang2112.dev@gmail.com>
2022-03-12 15:46:10 +1100

    Filter out empty words after string split for setrep

--- src/commands/reputation/setReputation.ts
+++ src/commands/reputation/setReputation.ts
@@ -20,7 +20,9 @@ export const setReputation = async (msg: Message) => {
   if (!discordUser) return; // return if user not found
   if (discordUser.bot) return; // return if mention bot
 
-  const splittedMessage = msg.content.split(' ');
+  const splittedMessage = msg.content
+    .split(' ')
+    .filter((word) => word.length > 0);
   if (splittedMessage.length !== 3) {
     return channel.send(
       'Wrong format. The correct format is `setRep @username repNumber`'
