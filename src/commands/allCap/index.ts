6541ca9b45d3869c0449907076bfa9495f66c3c3
ThanhNguyen <nnct2000@gmail.com>
2022-03-21 18:50:51 +1100

    Refactor allCap

--- src/commands/allCap/index.ts
+++ src/commands/allCap/index.ts
@@ -1,9 +1,14 @@
 import { Message, TextChannel } from 'discord.js';
 import {
-  isBlank,
-  fetchMessageById,
   fetchLastMessageBeforeId,
+  fetchMessageById,
+  isBlank,
 } from '../../utils';
+
+type GetChatContentPayload = Pick<
+  Message,
+  'content' | 'reference' | 'id' | 'channel'
+>;
 
 const generateAllCapText = (message: string) =>
   message
@@ -14,6 +19,31 @@ const generateAllCapText = (message: string) =>
       return `${outputText + character} `;
     }, '');
 
+const getChatContent = async ({
+  content,
+  channel,
+  reference,
+  id,
+}: GetChatContentPayload) => {
+  const cmdSpaceIndex = content.trimEnd().indexOf(' ');
+  const textChannel = channel as TextChannel;
+
+  if (cmdSpaceIndex !== -1) {
+    return content.slice(cmdSpaceIndex);
+  }
+
+  if (reference?.messageId) {
+    const chatContent = await fetchMessageById(
+      textChannel,
+      reference.messageId
+    );
+    return chatContent;
+  }
+
+  const chatContent = await fetchLastMessageBeforeId(textChannel, id);
+  return chatContent;
+};
+
 export const allCapExpandText = async ({
   content,
   channel,
@@ -23,28 +53,7 @@ export const allCapExpandText = async ({
 }: Message) => {
   if (author.bot) return; // return if sender is bot
 
-  const cmdSpaceIndex = content.trimEnd().indexOf(' ');
-
-  let chatContent = cmdSpaceIndex !== -1 ? content.slice(cmdSpaceIndex) : '';
-
-  // If -allcap is detected and it has content
-  if (!isBlank(chatContent)) {
-    const mockText = generateAllCapText(chatContent);
-    channel.send(mockText);
-    return;
-  }
-
-  // If -allcap is detected but content is blank...
-  if (reference && reference.messageId !== null) {
-    // and it's referring to another message, fetch that message
-    chatContent = await fetchMessageById(
-      channel as TextChannel,
-      reference.messageId!
-    );
-  } else {
-    // fetch the previous message in the channel
-    chatContent = await fetchLastMessageBeforeId(channel as TextChannel, id);
-  }
+  const chatContent = await getChatContent({ content, channel, reference, id });
 
   // If it's still blank at this point, then exit
   if (isBlank(chatContent)) {
