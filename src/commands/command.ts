4f1fabec5ee7a0382c8f36380bd94725323570d0
Sam Huynh <samhwang2112.dev@gmail.com>
2022-04-19 00:09:38 +1000

    Move deploy-command function into a command file
    So it can be run on the main process in production build

--- src/commands/command.ts
+++ src/commands/command.ts
@@ -3,6 +3,8 @@ import {
   SlashCommandSubcommandsOnlyBuilder,
 } from '@discordjs/builders';
 import { CommandInteraction } from 'discord.js';
+import { REST } from '@discordjs/rest';
+import { Routes } from 'discord-api-types/v10';
 
 export interface Command {
   data:
@@ -10,3 +12,27 @@ export interface Command {
     | SlashCommandSubcommandsOnlyBuilder;
   execute: (interaction: CommandInteraction) => Promise<void>;
 }
+
+interface ConfigType {
+  token: string;
+  clientId: string;
+  guildId: string;
+}
+
+export const deployCommands = async (
+  commandList: Command[],
+  config: ConfigType
+) => {
+  const { token, clientId, guildId } = config;
+  const commands = commandList.map((cmd) => cmd.data.toJSON());
+
+  const rest = new REST({ version: '10' }).setToken(token);
+  try {
+    await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+      body: commands,
+    });
+    console.log('Successfully register slash commands');
+  } catch (error: any) {
+    throw new Error('Cannot deploy commands', error.toString());
+  }
+};
--- src/deploy-command.ts
+++ src/deploy-command.ts
@@ -1,28 +1,18 @@
 import 'dotenv/config';
-import { SlashCommandBuilder } from '@discordjs/builders';
-import { REST } from '@discordjs/rest';
-import { Routes } from 'discord-api-types/v10';
+import { deployCommands } from './commands/command';
+import { commandList } from './commands';
 
 const deploy = async () => {
   const token = process.env.TOKEN ?? '';
   const clientId = process.env.CLIENT_ID ?? '';
   const guildId = process.env.GUILD_ID ?? '';
 
-  const pingCommand = new SlashCommandBuilder()
-    .setName('ping')
-    .setDescription('Replies with pong!');
-
-  const commands = [pingCommand].map((cmd) => cmd.toJSON());
-
-  const rest = new REST({ version: '10' }).setToken(token);
   try {
-    await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
-      body: commands,
-    });
-    console.log('Successfully register slash commands');
+    await deployCommands(commandList, { token, clientId, guildId });
   } catch (error) {
     console.error('Cannot deploy commands', error);
   }
 };
 
 deploy();
+process.on('SIGTERM', () => process.exit());
