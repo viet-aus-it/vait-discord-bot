c5d3d78c9d0e9d3654436fbb10b05f6387518c5e
Sam Huynh <samhwang2112.dev@gmail.com>
2022-04-19 00:09:38 +1000

    Move deploy-command function into a command file
    So it can be run on the main process in production build

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { processMessage } from './utils';
 import { getDiscordClient } from './clients';
 import { getConfigs } from './config';
 import { commandList } from './commands';
+import { deployCommands } from './commands/command';
 
 const main = async () => {
   const token = process.env.TOKEN ?? '';
@@ -11,6 +12,16 @@ const main = async () => {
 
   if (!client.user) throw new Error('Something went wrong!');
   console.log(`Logged in as ${client.user.tag}!`);
+
+  if (process.env.NODE_ENV === 'production') {
+    // This should only be run once during the bot startup in production.
+    // For development usage, please use `pnpm deploy:command`
+    await deployCommands(commandList, {
+      token,
+      guildId: 'placeholder_remove_later',
+      clientId: 'placeholder_remove_later',
+    });
+  }
 
   const configs = getConfigs(client.user);
   client.on('messageCreate', (msg) => {
