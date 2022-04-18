45e977483c9d4ee2cf0c066388fd9d484a25ce48
Sam Huynh <samhwang2112.dev@gmail.com>
2022-04-18 18:33:03 +1000

    Add a script to deploy slash commands

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,30 @@ import { getDiscordClient } from './clients';
 import { getConfigs } from './config';
 
 const main = async () => {
-  const client = await getDiscordClient({ token: process.env.TOKEN });
+  const token = process.env.TOKEN ?? '';
+  const client = await getDiscordClient({ token });
 
   if (!client.user) throw new Error('Something went wrong!');
   console.log(`Logged in as ${client.user.tag}!`);
 
   const configs = getConfigs(client.user);
-  client.on('messageCreate', (msg) => processMessage(msg, configs));
+  client.on('messageCreate', (msg) => {
+    if (msg.type === 'APPLICATION_COMMAND') return;
+
+    return processMessage(msg, configs);
+  });
+
+  client.on('interactionCreate', async (interaction) => {
+    const isCommand = interaction.isCommand();
+
+    if (!isCommand) return;
+
+    const { commandName } = interaction;
+    console.log({ commandName });
+    if (commandName === 'ping') {
+      await interaction.reply('pong!');
+    }
+  });
 };
 
 main();
