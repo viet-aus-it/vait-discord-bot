a9aed3b8dcd3baf804e5dea01109c7a631c49717
Sam Huynh <samhwang2112.dev@gmail.com>
2023-03-06 11:18:56 +1100

    Add script to delete global commands if deployed by accident

---
+++ src/scripts/delete-global-commands.ts
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+import dotenvExpand from 'dotenv-expand';
+import { deployGlobalCommands } from '../commands/command';
+
+const env = dotenv.config();
+dotenvExpand.expand(env);
+
+const deploy = async () => {
+  const token = process.env.TOKEN ?? '';
+  const clientId = process.env.CLIENT_ID ?? '';
+  const guildId = process.env.GUILD_ID ?? '';
+
+  try {
+    await deployGlobalCommands([], { token, clientId, guildId });
+    process.exit();
+  } catch (error) {
+    console.error('Cannot delete commands', error);
+  }
+};
+
+deploy();
+process.on('SIGTERM', () => process.exit());
