ab43499a5d79fb447d72d963d5657ca0f0da6686
Hieu Huynh <hieu@tiikr.com>
2023-03-26 21:01:28 +1000

    Updated the onboarding.sh script to include client id, public key and guild id.

--- scripts/onboarding.sh
+++ scripts/onboarding.sh
@@ -8,9 +8,28 @@ else
   echo "Token registered. This will be filled in your local env file."
 fi
 
+# Function to prompt user for input and set up environment variable
+function prompt_and_set {
+  read -p "$1" value
+  if [ -z "$value" ]; then
+    echo "Please fill in the $2 field in the .env file later."
+  else
+    echo "$2 registered. This will be filled in your local env file."
+    export "$2"="$value"
+  fi
+}
+
+# Ask user for bot token, client id, guild id and public key
+prompt_and_set "Please enter your client id if you have one, or leave it blank:" CLIENT_ID
+prompt_and_set "Please enter your bot public key if you have one, or leave it blank:" PUBLIC_KEY
+prompt_and_set "Please enter your guild id if you have one, or leave it blank:" GUILD_ID
+
 echo "Creating env files for discord bot"
 cp .env.dist .env
 sed -i'' -e "s/TOKEN=YOUR_BOT_TOKEN_HERE/TOKEN=${BOT_TOKEN}/g" .env
+sed -i'' -e "s/CLIENT_ID=CLIENT_ID_GOES_HERE/CLIENT_ID=${CLIENT_ID}/g" .env
+sed -i'' -e "s/PUBLIC_KEY=PUBLIC_KEY_GOES_HERE/PUBLIC_KEY=${PUBLIC_KEY}/g" .env
+sed -i'' -e "s/#GUILD_ID=FOR_DEVELOPMENT_ONLY/GUILD_ID=${GUILD_ID}/g" .env
 
 NODE_VER=$(node --version)
 PNPM_VER=$(pnpm --version)
