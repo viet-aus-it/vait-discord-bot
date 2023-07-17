a2128b36c116a37e5af1ad3a2c9c4554d1da6784
Sam Huynh <sam.huynh@lyka.com.au>
2023-07-17 12:06:24 +1000

    feat: refactor powerball
    - Move unique random int test into its own module
    - Merge array reduces into 1 call
    - Make test run with the actual command instead of the powerball func

--- src/commands/powerball/powerball.test.ts
+++ src/commands/powerball/powerball.test.ts
@@ -1,43 +1,62 @@
-import { getPowerBallGame } from '.';
-import { getUniqueRandomIntInclusive } from '../../utils';
-// /powerball n number (1-10)
+import { describe, it, expect, beforeEach } from 'vitest';
+import { mockDeep, mockReset, captor } from 'vitest-mock-extended';
+import { ChatInputCommandInteraction } from 'discord.js';
+import { powerball } from '.';
 
-// 1. 7 unique numbers from 1 - 35
-// 2. PB numbers from 1 - 20
-// 3. n number of games
-// 4. format
+const mockInteraction = mockDeep<ChatInputCommandInteraction>();
 
 describe('powerball', () => {
+  beforeEach(() => {
+    mockReset(mockInteraction);
+  });
+
   it('should return correct number of games', async () => {
     const gameCount = 5;
-    const games = getPowerBallGame(gameCount);
-    expect(games.split('\n').length).toEqual(gameCount);
+    mockInteraction.options.getInteger.mockReturnValueOnce(gameCount);
+    const games = captor<string>();
+
+    await powerball(mockInteraction);
+
+    expect(mockInteraction.reply).toHaveBeenCalledWith(games);
+    expect(games.value.split('\n').length).toEqual(gameCount);
   });
 
   it('should return 7 unique numbers from 1-35', async () => {
-    let singleGame = getPowerBallGame(1).replaceAll('`', '');
-    singleGame = singleGame.substring(0, singleGame.indexOf('P')).trim();
-    const uniqueNumbers = singleGame.split(' ');
+    mockInteraction.options.getInteger.mockReturnValueOnce(1);
+    const singleGame = captor<string>();
+
+    await powerball(mockInteraction);
+
+    expect(mockInteraction.reply).toHaveBeenCalledWith(singleGame);
+
+    let gameValue = singleGame.value.replaceAll('`', '');
+    gameValue = gameValue.substring(0, gameValue.indexOf('PB')).trim();
+    const uniqueNumbers = gameValue.split(' ');
     expect(uniqueNumbers.length).toEqual(new Set(uniqueNumbers).size);
   });
 
   it('should return the correct format', async () => {
-    const games = getPowerBallGame(10);
-    expect(games.match(/([0-9]{2}\s){7}\sPB: [0-9]{2}\n?/));
+    mockInteraction.options.getInteger.mockReturnValueOnce(10);
+    const games = captor<string>();
+    const regex = /^(\d{2}\s){7}PB:\d{2}$/gm; // Matches multiline of 00 00 00 00 00 00 00 PB:00
+
+    await powerball(mockInteraction);
+
+    expect(mockInteraction.reply).toHaveBeenCalledWith(games);
+    expect(games.value.replaceAll('`', '')).toMatch(regex);
   });
 
   it('PB numbers from 1 - 20', async () => {
-    const singleGame = getPowerBallGame(1).replaceAll('`', '');
-    const powerball = singleGame
-      .substring(singleGame.indexOf(':') + 1, singleGame.length)
-      .trim();
-    expect(Number(powerball)).lessThanOrEqual(20).greaterThanOrEqual(1);
-  });
-});
+    mockInteraction.options.getInteger.mockReturnValueOnce(1);
+    const singleGame = captor<string>();
 
-describe('Random unique number', () => {
-  it('should return a unique number that is not in the array', async () => {
-    const number = getUniqueRandomIntInclusive([1, 2, 3], 1, 4);
-    expect(number).toEqual(4);
+    await powerball(mockInteraction);
+
+    expect(mockInteraction.reply).toHaveBeenCalledWith(singleGame);
+
+    const gameValue = singleGame.value.replaceAll('`', '');
+    const pbNumber = gameValue.substring(gameValue.indexOf(':') + 1);
+    const pbValue = Number.parseInt(pbNumber, 10);
+    expect(pbValue).lessThanOrEqual(20).greaterThanOrEqual(1);
   });
 });
---
+++ src/utils/random/index.test.ts
@@ -0,0 +1,9 @@
+import { describe, it, expect } from 'vitest';
+import { getUniqueRandomIntInclusive } from '.';
+
+describe('Random unique number', () => {
+  it('should return a unique number that is not in the array', async () => {
+    const number = getUniqueRandomIntInclusive([1, 2, 3], 1, 4);
+    expect(number).toEqual(4);
+  });
+});
