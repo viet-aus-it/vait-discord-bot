7a944320679e421ccb69dace908e1a304cfeecb6
Sam Huynh <samhwang2112.dev@gmail.com>
2024-02-15 11:39:18 +0000

    feat: add a method for constructing weather location URL before sending

--- src/commands/weather/fetchWeather.ts
+++ src/commands/weather/fetchWeather.ts
@@ -1,14 +1,18 @@
 import wretch from 'wretch';
 
 export const WEATHER_URL = 'https://wttr.in/';
-const ARGUMENTS = '?0mMT';
+export const ARGUMENTS = '?0mMT';
+
+const constructWeatherLocation = (where: string) => {
+  return where.replaceAll(' ', '+') + ARGUMENTS;
+};
 
 const weatherApi = wretch(WEATHER_URL);
 
 export const fetchWeather = async (where: string) => {
   const response = await weatherApi
     .headers({ 'User-Agent': 'curl', 'Content-Type': 'text/plain' })
-    .get(where + ARGUMENTS)
+    .get(constructWeatherLocation(where))
     .text()
     .catch((err) => {
       throw new Error(`ERROR IN FETCHING WEATHER: ERROR ${err.status}`, err);
