c9400898d829024eddc841f73ff3738542b7bc6a
Sam Huynh <samhwang2112.dev@gmail.com>
2024-03-22 07:47:46 +1100

    feat: add axiom exporter

--- src/tracing.ts
+++ src/tracing.ts
@@ -1,18 +1,27 @@
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
 import { Resource } from '@opentelemetry/resources';
 import { ConsoleMetricExporter, PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { BatchSpanProcessor, ConsoleSpanExporter } from '@opentelemetry/sdk-trace-node';
 import { SEMRESATTRS_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
 
-const traceExporter = new ConsoleSpanExporter();
+const consoleTraceExporter = new ConsoleSpanExporter();
+const axiomTraceExporter = new OTLPTraceExporter({
+  url: 'https://api.axiom.co/v1/traces',
+  headers: {
+    Authorization: `Bearer ${process.env.AXIOM_TOKEN}`,
+    'X-Axiom-Dataset': process.env.AXIOM_DATASET,
+  },
+});
+const traceExporter = process.env.NODE_ENV === 'production' ? axiomTraceExporter : consoleTraceExporter;
+
 const resource = new Resource({
   [SEMRESATTRS_SERVICE_NAME]: 'discord-bot',
 });
 
 const sdk = new NodeSDK({
   spanProcessor: new BatchSpanProcessor(traceExporter),
-  traceExporter: new ConsoleSpanExporter(),
   metricReader: new PeriodicExportingMetricReader({
     exporter: new ConsoleMetricExporter(),
   }),
