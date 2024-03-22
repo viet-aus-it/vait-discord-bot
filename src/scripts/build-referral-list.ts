d85a271d4a348abca20507416a827b10e9a0dffa
Sam Huynh <samhwang2112.dev@gmail.com>
2024-03-23 00:01:08 +1100

    automate generate referrals (#216)
    * chore: install node-html-parser
    
    * feat: add a script to scrape the list of ozbargain services
    
    * feat: move build referral list script into src scripts
    
    * feat: add a function to create json data file
    
    * feat: clean up build referral list script
    
    - cleanup output dir before making new file
    - add logger in build methods
    
    * WIP: create github action to build ozbargain referrals
    
    * test:CI automate
    
    * update diff commands
    
    * test: use porcelain status
    
    * test: use production environment
    
    * test: link secret to env
    
    * delettest: d generated file to triggerigger diff
    
    * test: use github token with gh cli
    
    * chore: fix typo in file name
    
    * print out outout of git status --porcelain
    
    * test: trigger diff part 2
    
    * feat: update branch name in making PR
    
    * feat: explicit add before making PR
    
    * feat: remove the fill option and add explicit title and body
    
    * feat: fetch all git history
    
    * test: force push
    
    * test: cleanup testing
    
    * chore: regenerate ozbargain list

---
+++ src/scripts/build-referral-list.ts
@@ -0,0 +1,42 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import { type HTMLElement, parse as parseHtml } from 'node-html-parser';
+import wretch from 'wretch';
+import { logger } from '../utils/logger';
+
+const ozbargainApi = wretch('https://www.ozbargain.com.au/wiki/list_of_referral_links');
+const OUTPUT_DIR = path.join(__dirname, '..', 'commands', 'referral', 'generated');
+
+const getOzbReferralNodes = async () => {
+  logger.info('[get-ozbargain-referral-nodes]: Fetching Ozbargain referral list');
+  const rawHtml = await ozbargainApi.get().text();
+  const htmlTree = parseHtml(rawHtml);
+  const nodes = htmlTree.querySelectorAll('.level1');
+  logger.info('[get-ozbargain-referral-nodes]: Fetch Ozbargain referral list complete');
+  return nodes;
+};
+
+const cleanOutpitDir = () => {
+  logger.info('[clean-output-dir]: Cleaning output directory');
+  fs.rmSync(OUTPUT_DIR, { recursive: true });
+  fs.mkdirSync(OUTPUT_DIR);
+  logger.info('[clean-output-dir]: Output directory cleaned');
+};
+
+const buildOzbServicesFile = (nodes: HTMLElement[]) => {
+  logger.info('[build-ozbaragain-services-file]: Building Ozbargain referral list');
+  const content = JSON.stringify(nodes.map((node) => node.text.toLowerCase()));
+  const filePath = path.join(OUTPUT_DIR, 'ozbargain-services.json');
+  fs.writeFileSync(filePath, content);
+  logger.info('[build-ozbaragain-services-file]: Ozbargain referral list complete');
+};
+
+const build = async () => {
+  logger.info('[build-referral-list]: Building Ozbargain referral list');
+  const nodes = await getOzbReferralNodes();
+  cleanOutpitDir();
+  buildOzbServicesFile(nodes);
+  logger.info('[build-referral-list]: Ozbargain referral list complete');
+};
+
+build();
