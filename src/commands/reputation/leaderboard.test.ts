107bd1fe9c01dd123fbc609ac20dd0188902c1a1
Sam Huynh <samhwang2112.dev@gmail.com>
2024-03-27 21:24:12 +1100

    WIP: Flexible leaderboard

--- src/commands/reputation/leaderboard.test.ts
+++ src/commands/reputation/leaderboard.test.ts
@@ -1,11 +1,11 @@
 import type { ChatInputCommandInteraction } from 'discord.js';
 import { beforeEach, describe, expect, it, vi } from 'vitest';
 import { mockDeep, mockReset } from 'vitest-mock-extended';
-import { getTop10 } from './_helpers';
-import { getLeaderboard } from './leaderboard';
+import { getRepLeaderboard } from './_helpers';
+import { getLeaderboard as getLeaderboardCommand } from './leaderboard';
 
 vi.mock('./_helpers');
-const mockGetTop10 = vi.mocked(getTop10);
+const mockGetRepLeaderboard = vi.mocked(getRepLeaderboard);
 
 const mockInteraction = mockDeep<ChatInputCommandInteraction<'cached'>>();
 
@@ -15,18 +15,18 @@ describe('leaderboard', () => {
   });
 
   it('Should send reply error if it cannot retrieve the top 10', async () => {
-    mockGetTop10.mockResolvedValueOnce([]);
+    mockGetRepLeaderboard.mockResolvedValueOnce([]);
 
-    await getLeaderboard(mockInteraction);
+    await getLeaderboardCommand(mockInteraction);
 
-    expect(mockGetTop10).toHaveBeenCalledOnce();
+    expect(mockGetRepLeaderboard).toHaveBeenCalledOnce();
     expect(mockInteraction.reply).toHaveBeenCalledOnce();
     expect(mockInteraction.reply).toHaveBeenCalledWith('No one has rep to be on the leaderboard, yet.');
   });
 
   it('Should create a leaderboard entry with nickname if it can be fetched', async () => {
     const userid = '1';
-    mockGetTop10.mockResolvedValueOnce([
+    mockGetRepLeaderboard.mockResolvedValueOnce([
       {
         id: userid,
         reputation: 0,
@@ -47,9 +47,9 @@ describe('leaderboard', () => {
       },
     };
 
-    await getLeaderboard(interaction);
+    await getLeaderboardCommand(interaction);
 
-    expect(mockGetTop10).toHaveBeenCalledOnce();
+    expect(mockGetRepLeaderboard).toHaveBeenCalledOnce();
     expect(mockInteraction.reply).toHaveBeenCalledOnce();
     expect(mockInteraction.reply).toHaveBeenCalledWith(
       `\`\`\`
@@ -61,7 +61,7 @@ describe('leaderboard', () => {
 
   it('Should create a leaderboard entry with displayName if it can be fetched and nickname cannot be fetched', async () => {
     const userid = '1';
-    mockGetTop10.mockResolvedValueOnce([
+    mockGetRepLeaderboard.mockResolvedValueOnce([
       {
         id: userid,
         reputation: 0,
@@ -82,9 +82,9 @@ describe('leaderboard', () => {
       },
     };
 
-    await getLeaderboard(interaction);
+    await getLeaderboardCommand(interaction);
 
-    expect(mockGetTop10).toHaveBeenCalledOnce();
+    expect(mockGetRepLeaderboard).toHaveBeenCalledOnce();
     expect(mockInteraction.reply).toHaveBeenCalledOnce();
     expect(mockInteraction.reply).toHaveBeenCalledWith(
       `\`\`\`
@@ -96,7 +96,7 @@ describe('leaderboard', () => {
 
   it('Should create a leaderboard entry with userid if it cannot retrieve the member', async () => {
     const userid = '1';
-    mockGetTop10.mockResolvedValueOnce([
+    mockGetRepLeaderboard.mockResolvedValueOnce([
       {
         id: userid,
         reputation: 0,
@@ -117,9 +117,9 @@ describe('leaderboard', () => {
       },
     };
 
-    await getLeaderboard(interaction);
+    await getLeaderboardCommand(interaction);
 
-    expect(mockGetTop10).toHaveBeenCalledOnce();
+    expect(mockGetRepLeaderboard).toHaveBeenCalledOnce();
     expect(mockInteraction.reply).toHaveBeenCalledOnce();
     expect(mockInteraction.reply).toHaveBeenCalledWith(
       `\`\`\`
@@ -130,7 +130,7 @@ describe('leaderboard', () => {
   });
 
   it('Should send reply with leaderboard with more than 1 record in correct order', async () => {
-    mockGetTop10.mockResolvedValueOnce([
+    mockGetRepLeaderboard.mockResolvedValueOnce([
       {
         id: '3',
         reputation: 3,
@@ -168,9 +168,9 @@ describe('leaderboard', () => {
       },
     };
 
-    await getLeaderboard(interaction);
+    await getLeaderboardCommand(interaction);
 
-    expect(mockGetTop10).toHaveBeenCalledOnce();
+    expect(mockGetRepLeaderboard).toHaveBeenCalledOnce();
     expect(mockInteraction.reply).toHaveBeenCalledOnce();
     expect(mockInteraction.reply).toHaveBeenCalledWith(
       `\`\`\`
