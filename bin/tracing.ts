9fa1428b290efa2bbf9d8c84da87f75bde953ae2
Sam Huynh <samhwang2112.dev@gmail.com>
2024-07-27 09:41:43 +1000

    feat: enable tracing for Prisma
    # Conflicts:
    #	prisma/schema.prisma
    
    # Conflicts:
    #	package.json
    #	pnpm-lock.yaml

--- bin/tracing.ts
+++ bin/tracing.ts
@@ -5,6 +5,7 @@ import { registerInstrumentations } from '@opentelemetry/instrumentation';
 import { Resource } from '@opentelemetry/resources';
 import { BatchSpanProcessor, NodeTracerProvider, SimpleSpanProcessor, type SpanExporter, type SpanProcessor } from '@opentelemetry/sdk-trace-node';
 import { ATTR_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
+import { PrismaInstrumentation } from '@prisma/instrumentation';
 
 const SERVICE_NAME = 'discord-bot';
 
@@ -42,7 +43,7 @@ export function setupTracer() {
 
   registerInstrumentations({
     tracerProvider: provider,
-    instrumentations: [getNodeAutoInstrumentations()],
+    instrumentations: [getNodeAutoInstrumentations(), new PrismaInstrumentation({ middleware: true, enabled: true })],
   });
 
   process.on('SIGTERM', async () => {
