1db4bf1252c42405713eed926cf0dcaf739ff78a
Sam Huynh <samhwang2112.dev@gmail.com>
2024-07-26 23:47:25 +1000

    feat: add spans to entrypoints

--- bin/cleanup-expired-referrals.ts
+++ bin/cleanup-expired-referrals.ts
@@ -1,3 +1,4 @@
+import { SpanStatusCode, trace } from '@opentelemetry/api';
 import { Result } from 'oxide.ts';
 import { cleanupExpiredCode } from '../src/slash-commands/referral/utils';
 import { loadEnv } from '../src/utils/load-env';
@@ -7,15 +8,35 @@ import { setupTracer } from './tracing';
 const cleanup = async () => {
   loadEnv();
   setupTracer();
-  logger.info('[cleanup-expired-referrals]: CLEANING UP EXPIRED REFERRALS');
+  const tracer = trace.getTracer('discord-bot');
+  await tracer.startActiveSpan('cleanup-expired-referrals', async (span) => {
+    logger.info('[cleanup-expired-referrals]: CLEANING UP EXPIRED REFERRALS');
+    span.setStatus({
+      code: SpanStatusCode.UNSET,
+      message: 'Cleaning up expired referrals',
+    });
 
-  const op = await Result.safe(cleanupExpiredCode());
-  if (op.isErr()) {
-    logger.error('[cleanup-expired-referrals]: Error cleaning up expired referrals');
-    process.exit(1);
-  }
+    const op = await Result.safe(cleanupExpiredCode());
+    if (op.isErr()) {
+      logger.error('[cleanup-expired-referrals]: Error cleaning up expired referrals');
+      span.setStatus({
+        code: SpanStatusCode.ERROR,
+        message: 'Error cleaning up expired referrals',
+      });
+      span.setAttributes({
+        'app.error': op.unwrapErr().toString(),
+      });
 
-  logger.info('[cleanup-expired-referrals]: Removed expired referrals');
+      process.exit(1);
+    }
+
+    logger.info('[cleanup-expired-referrals]: Removed expired referrals');
+    span.setStatus({
+      code: SpanStatusCode.OK,
+      message: 'Removed expired referrals',
+    });
+  });
+
   process.exit(0);
 };
 
