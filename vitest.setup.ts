baa0a09bf3004ea4f8c53821df4bd342aa7d0922
Sam Huynh <samhwang2112.dev@gmail.com>
2024-12-12 00:31:56 +1100

    feat: replace winston with pino

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,10 +1,8 @@
 import { afterAll, afterEach, beforeAll } from 'vitest';
 import { server } from './mocks/server';
-import { logger } from './src/utils/logger';
 
 beforeAll(() => {
   server.listen();
-  logger.silent = true;
 });
 
 afterEach(() => {
@@ -13,5 +11,4 @@ afterEach(() => {
 
 afterAll(() => {
   server.close();
-  logger.silent = false;
 });
