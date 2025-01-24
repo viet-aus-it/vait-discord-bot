cc95194b2fba68e325a36215f70d55f05a94ce36
Sam Huynh <samhwang2112.dev@gmail.com>
2025-01-25 04:24:03 +0700

    test: update testcontainer setup message

--- mocks/database/globalSetup.ts
+++ mocks/database/globalSetup.ts
@@ -2,7 +2,7 @@ import childProcess from 'node:child_process';
 import dotenv from '@dotenvx/dotenvx';
 import { PostgreSqlContainer } from '@testcontainers/postgresql';
 
-function migrateDb() {
+function setupDb() {
   console.log('Running Prisma Migrate');
   childProcess.execSync('pnpm run prisma:migrate');
   console.log('Prisma Migrate ran');
@@ -12,11 +12,11 @@ async function localSetup() {
   console.log('Starting Database');
   const db = await new PostgreSqlContainer('postgres:17-alpine').start();
   const databaseUrl = db.getConnectionUri();
+  console.log('Test container db url', databaseUrl);
   process.env.DATABASE_URL = databaseUrl;
-  console.log('Database started', databaseUrl);
-  console.log('ENV DB', process.env.DATABASE_URL);
+  console.log('Database is running at', process.env.DATABASE_URL);
 
-  migrateDb();
+  setupDb();
 
   return async function teardown() {
     console.log('Stopping Database');
@@ -27,9 +27,9 @@ async function localSetup() {
 
 async function ciSetup() {
   dotenv.config();
-  console.log('Database is running at ', process.env.DATABASE_URL);
+  console.log('Database is running at', process.env.DATABASE_URL);
 
-  migrateDb();
+  setupDb();
 
   return async function teardown() {
     console.log('Tearing down db...');
