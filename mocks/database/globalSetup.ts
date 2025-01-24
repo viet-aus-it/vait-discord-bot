e392ba9201d28c275e1a09bcbc27303587d85e20
Sam Huynh <samhwang2112.dev@gmail.com>
2025-01-25 03:56:50 +0700

    test: split up migrate and setup DB in a separate step

--- mocks/database/globalSetup.ts
+++ mocks/database/globalSetup.ts
@@ -1,21 +1,47 @@
 import childProcess from 'node:child_process';
+import dotenv from '@dotenvx/dotenvx';
 import { PostgreSqlContainer } from '@testcontainers/postgresql';
 
-export async function setup() {
+function migrateDb() {
+  console.log('Running Prisma Migrate');
+  childProcess.execSync('pnpm run prisma:migrate');
+  console.log('Prisma Migrate ran');
+}
+
+async function localSetup() {
   console.log('Starting Database');
-  const db = await new PostgreSqlContainer('postgres:15-alpine').start();
+  const db = await new PostgreSqlContainer('postgres:17-alpine').start();
   const databaseUrl = db.getConnectionUri();
   process.env.DATABASE_URL = databaseUrl;
   console.log('Database started', databaseUrl);
   console.log('ENV DB', process.env.DATABASE_URL);
 
-  console.log('Running Prisma Migrate');
-  childProcess.execSync('pnpm run prisma:migrate');
-  console.log('Prisma Migrate ran');
+  migrateDb();
 
   return async function teardown() {
     console.log('Stopping Database');
     await db.stop();
     console.log('Database stopped');
   };
+}
+
+async function ciSetup() {
+  dotenv.config();
+  console.log('Database is running at ', process.env.DATABASE_URL);
+
+  migrateDb();
+
+  return async function teardown() {
+    console.log('Tearing down db...');
+  };
+}
+
+export async function setup() {
+  const isInCI = !!process.env.CI;
+
+  if (isInCI) {
+    return ciSetup();
+  }
+
+  return localSetup();
 }
