cf400fec6c1e59ec35190fea521e281ef05d98c3
Sam Huynh <samhwang2112.dev@gmail.com>
2025-02-17 21:04:26 +1100

    test: load database url as an env in prisma

--- mocks/database/globalSetup.ts
+++ mocks/database/globalSetup.ts
@@ -2,9 +2,18 @@ import childProcess from 'node:child_process';
 import { PostgreSqlContainer } from '@testcontainers/postgresql';
 import { loadEnv } from '../../src/utils/load-env';
 
-function setupDb() {
+function setupDb(databaseUrl?: string) {
   console.log('Running Prisma Migrate');
-  childProcess.execSync('pnpm run prisma:migrate');
+  if (databaseUrl) {
+    process.env.DATABASE_URL = databaseUrl;
+  }
+  childProcess.execSync('pnpm run prisma:migrate', {
+    env: {
+      ...process.env,
+      DATABASE_URL: process.env.DATABASE_URL,
+      DIRECT_DATABASE_URL: process.env.DATABASE_URL,
+    },
+  });
   console.log('Prisma Migrate ran');
 }
 
@@ -12,11 +21,9 @@ async function localSetup() {
   console.log('Starting Database');
   const db = await new PostgreSqlContainer('postgres:17-alpine').start();
   const databaseUrl = db.getConnectionUri();
-  console.log('Test container db url', databaseUrl);
-  process.env.DATABASE_URL = databaseUrl;
-  console.log('Database is running at', process.env.DATABASE_URL);
+  console.log('Database is running at', databaseUrl);
 
-  setupDb();
+  setupDb(databaseUrl);
 
   return async function teardown() {
     console.log('Stopping Database');
