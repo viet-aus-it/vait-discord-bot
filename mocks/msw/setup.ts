e4c599d052f481ff7c94c58ca3d45979bd5a4a27
Sam Huynh <samhwang2112.dev@gmail.com>
2025-02-18 09:35:08 +1100

    remove msw bypass

--- mocks/msw/setup.ts
+++ mocks/msw/setup.ts
@@ -2,7 +2,7 @@ import { afterAll, afterEach, beforeAll } from 'vitest';
 import { server } from './server';
 
 beforeAll(() => {
-  server.listen({ onUnhandledRequest: 'bypass' });
+  server.listen();
 });
 
 afterEach(() => {
