6285a9bcbc7864a63ee5f5b9960f9aa8f00d455b
Sam Huynh <samhwang2112.dev@gmail.com>
2025-03-01 18:11:59 +1100

    fix: update test snapshot

--- src/slash-commands/referral/referral-new.test.ts
+++ src/slash-commands/referral/referral-new.test.ts
@@ -50,29 +50,37 @@ describe('autocomplete', () => {
 
     expect(mockAutocompleteInteraction.respond).toBeCalledWith(respondInput);
     expect(respondInput.value).toMatchInlineSnapshot(`
-    [
-      {
-        "name": "ahm health insurance",
-        "value": "ahm health insurance",
-      },
-      {
-        "name": "doctors' health fund",
-        "value": "doctors' health fund",
-      },
-      {
-        "name": "phoenix health fund",
-        "value": "phoenix health fund",
-      },
-      {
-        "name": "qantas insurance - health insurance",
-        "value": "qantas insurance - health insurance",
-      },
-      {
-        "name": "westfund health insurance",
-        "value": "westfund health insurance",
-      },
-    ]
-  `);
+      [
+        {
+          "name": "ahm health insurance",
+          "value": "ahm health insurance",
+        },
+        {
+          "name": "doctors health by avant",
+          "value": "doctors health by avant",
+        },
+        {
+          "name": "phoenix health fund",
+          "value": "phoenix health fund",
+        },
+        {
+          "name": "qantas insurance - health insurance",
+          "value": "qantas insurance - health insurance",
+        },
+        {
+          "name": "queensland country health fund",
+          "value": "queensland country health fund",
+        },
+        {
+          "name": "teachers health",
+          "value": "teachers health",
+        },
+        {
+          "name": "westfund health insurance",
+          "value": "westfund health insurance",
+        },
+      ]
+    `);
   });
 });
 
--- src/slash-commands/referral/services.test.ts
+++ src/slash-commands/referral/services.test.ts
@@ -6,28 +6,36 @@ describe('Service search test', () => {
     const options = searchServices('heal');
 
     expect(options).toMatchInlineSnapshot(`
-    [
-      {
-        "name": "ahm health insurance",
-        "value": "ahm health insurance",
-      },
-      {
-        "name": "doctors' health fund",
-        "value": "doctors' health fund",
-      },
-      {
-        "name": "phoenix health fund",
-        "value": "phoenix health fund",
-      },
-      {
-        "name": "qantas insurance - health insurance",
-        "value": "qantas insurance - health insurance",
-      },
-      {
-        "name": "westfund health insurance",
-        "value": "westfund health insurance",
-      },
-    ]
-  `);
+      [
+        {
+          "name": "ahm health insurance",
+          "value": "ahm health insurance",
+        },
+        {
+          "name": "doctors health by avant",
+          "value": "doctors health by avant",
+        },
+        {
+          "name": "phoenix health fund",
+          "value": "phoenix health fund",
+        },
+        {
+          "name": "qantas insurance - health insurance",
+          "value": "qantas insurance - health insurance",
+        },
+        {
+          "name": "queensland country health fund",
+          "value": "queensland country health fund",
+        },
+        {
+          "name": "teachers health",
+          "value": "teachers health",
+        },
+        {
+          "name": "westfund health insurance",
+          "value": "westfund health insurance",
+        },
+      ]
+    `);
   });
 });
