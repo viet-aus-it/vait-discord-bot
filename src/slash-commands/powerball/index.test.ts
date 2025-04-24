d0aabd255d90b77af2d7e485ae84a936a13301b8
Sam Huynh <samhwang2112.dev@gmail.com>
2025-04-24 23:20:29 +1000

    feat: use the chat input command interaction fixture in simple tests

--- src/slash-commands/powerball/index.test.ts
+++ src/slash-commands/powerball/index.test.ts
@@ -1,33 +1,27 @@
-import type { ChatInputCommandInteraction } from 'discord.js';
-import { beforeEach, describe, expect, it } from 'vitest';
-import { captor, mockDeep, mockReset } from 'vitest-mock-extended';
+import { describe, expect } from 'vitest';
+import { captor } from 'vitest-mock-extended';
 import { powerball } from '.';
-
-const mockInteraction = mockDeep<ChatInputCommandInteraction>();
+import { chatInputCommandInteractionTest } from '../../../test/fixtures/chat-input-command-interaction';
 
 describe('powerball', () => {
-  beforeEach(() => {
-    mockReset(mockInteraction);
-  });
-
-  it('should return correct number of games', async () => {
+  chatInputCommandInteractionTest('should return correct number of games', async ({ interaction }) => {
     const gameCount = 5;
-    mockInteraction.options.getInteger.mockReturnValueOnce(gameCount);
+    interaction.options.getInteger.mockReturnValueOnce(gameCount);
     const games = captor<string>();
 
-    await powerball(mockInteraction);
+    await powerball(interaction);
 
-    expect(mockInteraction.reply).toHaveBeenCalledWith(games);
+    expect(interaction.reply).toHaveBeenCalledWith(games);
     expect(games.value.split('\n').length).toEqual(gameCount);
   });
 
-  it('should return 7 unique numbers from 1-35', async () => {
-    mockInteraction.options.getInteger.mockReturnValueOnce(1);
+  chatInputCommandInteractionTest('should return 7 unique numbers from 1-35', async ({ interaction }) => {
+    interaction.options.getInteger.mockReturnValueOnce(1);
     const singleGame = captor<string>();
 
-    await powerball(mockInteraction);
+    await powerball(interaction);
 
-    expect(mockInteraction.reply).toHaveBeenCalledWith(singleGame);
+    expect(interaction.reply).toHaveBeenCalledWith(singleGame);
 
     let gameValue = singleGame.value.replaceAll('`', '');
     gameValue = gameValue.substring(0, gameValue.indexOf('PB')).trim();
@@ -35,24 +29,24 @@ describe('powerball', () => {
     expect(uniqueNumbers.length).toEqual(new Set(uniqueNumbers).size);
   });
 
-  it('should return the correct format', async () => {
-    mockInteraction.options.getInteger.mockReturnValueOnce(10);
+  chatInputCommandInteractionTest('should return the correct format', async ({ interaction }) => {
+    interaction.options.getInteger.mockReturnValueOnce(10);
     const games = captor<string>();
     const regex = /^(\d{2}\s){7}PB:\d{2}$/gm; // Matches multiline of 00 00 00 00 00 00 00 PB:00
 
-    await powerball(mockInteraction);
+    await powerball(interaction);
 
-    expect(mockInteraction.reply).toHaveBeenCalledWith(games);
+    expect(interaction.reply).toHaveBeenCalledWith(games);
     expect(games.value.replaceAll('`', '')).toMatch(regex);
   });
 
-  it('PB numbers from 1 - 20', async () => {
-    mockInteraction.options.getInteger.mockReturnValueOnce(1);
+  chatInputCommandInteractionTest('PB numbers from 1 - 20', async ({ interaction }) => {
+    interaction.options.getInteger.mockReturnValueOnce(1);
     const singleGame = captor<string>();
 
-    await powerball(mockInteraction);
+    await powerball(interaction);
 
-    expect(mockInteraction.reply).toHaveBeenCalledWith(singleGame);
+    expect(interaction.reply).toHaveBeenCalledWith(singleGame);
 
     const gameValue = singleGame.value.replaceAll('`', '');
     const pbNumber = gameValue.substring(gameValue.indexOf(':') + 1);
--- test/fixtures/chat-input-command-interaction.ts
+++ test/fixtures/chat-input-command-interaction.ts
@@ -1,19 +1,31 @@
 import { faker } from '@faker-js/faker';
-import type { ChatInputCommandInteraction } from 'discord.js';
+import type { ChatInputCommandInteraction, Message } from 'discord.js';
 import { test } from 'vitest';
 import { type DeepMockProxy, mockDeep, mockReset } from 'vitest-mock-extended';
 
 interface ChatInputCommandInteractionTest {
   interaction: DeepMockProxy<ChatInputCommandInteraction>;
+  message: DeepMockProxy<Message<true>>;
 }
+
+const interaction = mockDeep<ChatInputCommandInteraction>();
+const message = mockDeep<Message<true>>();
+
+const guildId = faker.string.nanoid();
 
 export const chatInputCommandInteractionTest = test.extend<ChatInputCommandInteractionTest>({
   interaction: async ({}, use) => {
-    const interaction = mockDeep<ChatInputCommandInteraction>();
-    interaction.guildId = faker.string.nanoid();
+    interaction.guildId = guildId;
 
     await use(interaction);
 
     mockReset(interaction);
+  },
+  message: async ({}, use) => {
+    message.guildId = guildId;
+
+    await use(message);
+
+    mockReset(message);
   },
 });
