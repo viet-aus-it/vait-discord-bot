88e3afbfbe839e5d6bc4a509885134a0e15d4620
Sam Huynh <samhwang2112.dev@gmail.com>
2025-04-25 00:16:18 +1000

    chore: undo reformat aoc leaderboard test

--- src/slash-commands/aoc-leaderboard/index.test.ts
+++ src/slash-commands/aoc-leaderboard/index.test.ts
@@ -18,40 +18,45 @@ const mockLeaderboardId = faker.string.alphanumeric();
 
 const mockSystemTime = new Date(2024, 11, 25, 16, 0, 0); // 25/12/2024 16:00:00
 
-describe('Get AOC Leaderboard Utilities', () => {
+describe('Get AOC Leaderboard test', () => {
   beforeEach(() => {
-    vi.useFakeTimers();
-    vi.setSystemTime(mockSystemTime);
+    mockReset(mockInteraction);
   });
 
-  afterEach(() => {
-    vi.runAllTimers();
-    vi.useRealTimers();
-  });
-
-  describe('Get AOC Year', () => {
-    it('Should return this year leaderboard if it is December', () => {
-      const date = new Date(2024, 11, 1);
-      vi.setSystemTime(date);
-      const year = getAocYear();
-      expect(year).toEqual(2024);
+  describe('Static time tests', () => {
+    beforeEach(() => {
+      vi.useFakeTimers();
+      vi.setSystemTime(mockSystemTime);
     });
 
-    it('Should return previous year leaderboard if it is not December', () => {
-      const date = new Date(2024, 1, 1);
-      vi.setSystemTime(date);
-      const year = getAocYear();
-      expect(year).toEqual(2023);
+    afterEach(() => {
+      vi.runAllTimers();
+      vi.useRealTimers();
     });
-  });
 
-  describe('Format leaderboard', () => {
-    it('Should match the leaderboard format', () => {
-      const leaderboardMessage = formatLeaderboard({
-        result: parsedMockData,
-        updatedAt: mockSystemTime,
+    describe('Get AOC Year', () => {
+      it('Should return this year leaderboard if it is December', () => {
+        const date = new Date(2024, 11, 1);
+        vi.setSystemTime(date);
+        const year = getAocYear();
+        expect(year).toEqual(2024);
       });
-      expect(leaderboardMessage).toEqual(`\`\`\`
+
+      it('Should return previous year leaderboard if it is not December', () => {
+        const date = new Date(2024, 1, 1);
+        vi.setSystemTime(date);
+        const year = getAocYear();
+        expect(year).toEqual(2023);
+      });
+    });
+
+    describe('Format leaderboard', () => {
+      it('Should match the leaderboard format', () => {
+        const leaderboardMessage = formatLeaderboard({
+          result: parsedMockData,
+          updatedAt: mockSystemTime,
+        });
+        expect(leaderboardMessage).toEqual(`\`\`\`
  #               name score
  1 (anonymous user 4) 474
  2              user2 361
@@ -61,24 +66,21 @@ describe('Get AOC Leaderboard Utilities', () => {
 
 Last updated at: 25/12/2024 16:00
 \`\`\``);
+      });
     });
   });
-});
 
-describe('Get AOC Leaderboard Command', () => {
-  beforeEach(() => {
-    mockReset(mockInteraction);
-  });
-  it('Should reply with saved leaderboard if it can get one', async () => {
-    const guildId = faker.string.nanoid();
-    await saveLeaderboard(guildId, parsedMockData);
-    mockInteraction.guildId = guildId;
+  describe('Command tests', () => {
+    it('Should reply with saved leaderboard if it can get one', async () => {
+      const guildId = faker.string.nanoid();
+      await saveLeaderboard(guildId, parsedMockData);
+      mockInteraction.guildId = guildId;
 
-    await execute(mockInteraction);
+      await execute(mockInteraction);
 
-    const message = captor<string>();
-    expect(mockInteraction.editReply).toHaveBeenCalledWith(message);
-    expect(message.value).toContain(`
+      const message = captor<string>();
+      expect(mockInteraction.editReply).toHaveBeenCalledWith(message);
+      expect(message.value).toContain(`
  #               name score
  1 (anonymous user 4) 474
  2              user2 361
@@ -86,34 +88,35 @@ describe('Get AOC Leaderboard Command', () => {
  4              user1   0
 `);
 
-    await deleteLeaderboard(guildId);
-  });
+      await deleteLeaderboard(guildId);
+    });
 
-  it('Should reply with error if server is not configured', async () => {
-    const guildId = faker.string.nanoid();
-    mockInteraction.guildId = guildId;
+    it('Should reply with error if server is not configured', async () => {
+      const guildId = faker.string.nanoid();
+      mockInteraction.guildId = guildId;
 
-    await execute(mockInteraction);
+      await execute(mockInteraction);
 
-    expect(mockInteraction.editReply).toHaveBeenCalledWith('ERROR: Server is not configured to get AOC results! Missing Key and/or Leaderboard ID.');
-  });
+      expect(mockInteraction.editReply).toHaveBeenCalledWith('ERROR: Server is not configured to get AOC results! Missing Key and/or Leaderboard ID.');
+    });
 
-  it('Should reply with newly fetched leaderboard after fetching and saving', async () => {
-    const guildId = faker.string.nanoid();
-    await setAocSettings(guildId, mockKey, mockLeaderboardId);
-    mockFetchLeaderboard.mockResolvedValueOnce(parsedMockData);
-    mockInteraction.guildId = guildId;
+    it('Should reply with newly fetched leaderboard after fetching and saving', async () => {
+      const guildId = faker.string.nanoid();
+      await setAocSettings(guildId, mockKey, mockLeaderboardId);
+      mockFetchLeaderboard.mockResolvedValueOnce(parsedMockData);
+      mockInteraction.guildId = guildId;
 
-    await execute(mockInteraction);
+      await execute(mockInteraction);
 
-    const message = captor<string>();
-    expect(mockInteraction.editReply).toHaveBeenCalledWith(message);
-    expect(message.value).toContain(`
+      const message = captor<string>();
+      expect(mockInteraction.editReply).toHaveBeenCalledWith(message);
+      expect(message.value).toContain(`
  #               name score
  1 (anonymous user 4) 474
  2              user2 361
  3              user3 353
  4              user1   0
 `);
+    });
   });
 });
