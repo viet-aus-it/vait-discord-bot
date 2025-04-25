cd026456b3ee38ad415d2304eb040fa91993b8f7
Sam Huynh <samhwang2112.dev@gmail.com>
2025-04-25 21:15:11 +1000

    test: add fixture to moderate user

--- src/slash-commands/moderate-users/index.test.ts
+++ src/slash-commands/moderate-users/index.test.ts
@@ -1,32 +1,19 @@
-import {
-  type APIRole,
-  type ChatInputCommandInteraction,
-  Collection,
-  type GuildTextBasedChannel,
-  type Role,
-  type ThreadMember,
-  type ThreadMemberManager,
-} from 'discord.js';
-import { beforeEach, describe, expect, it } from 'vitest';
-import { type DeepMockProxy, mockDeep, mockReset } from 'vitest-mock-extended';
+import { type APIRole, Collection, type GuildTextBasedChannel, type Role, type ThreadMember, type ThreadMemberManager } from 'discord.js';
+import { describe, expect } from 'vitest';
+import type { DeepMockProxy } from 'vitest-mock-extended';
 import { removeUserByRole } from '.';
-
-const mockInteraction = mockDeep<ChatInputCommandInteraction>();
+import { chatInputCommandInteractionTest } from '../../../test/fixtures/chat-input-command-interaction';
 
 describe('Remove users who have the role', () => {
-  beforeEach(() => {
-    mockReset(mockInteraction);
+  chatInputCommandInteractionTest('should reply with an error message if the command is not executed in a thread', async ({ interaction }) => {
+    interaction.channel?.isThread.mockReturnValueOnce(false);
+
+    await removeUserByRole(interaction);
+
+    expect(interaction.reply).toHaveBeenCalledWith("You can't remove all users from entire channel. This command only works in a thread.");
   });
 
-  it('should reply with an error message if the command is not executed in a thread', async () => {
-    mockInteraction.channel?.isThread.mockReturnValueOnce(false);
-
-    await removeUserByRole(mockInteraction);
-
-    expect(mockInteraction.reply).toHaveBeenCalledWith("You can't remove all users from entire channel. This command only works in a thread.");
-  });
-
-  it('should remove all users from the thread with the matching role', async () => {
+  chatInputCommandInteractionTest('should remove all users from the thread with the matching role', async ({ interaction }) => {
     const fakeRoles = new Collection<string, Role>();
     fakeRoles.set('1', { id: 'role-id', name: 'name' } as Role);
     const mockMembers = new Collection<string, ThreadMember>();
@@ -55,17 +42,17 @@ describe('Remove users who have the role', () => {
       },
     } as ThreadMember);
 
-    mockInteraction.channel?.isTextBased.mockReturnValueOnce(true);
-    mockInteraction.channel?.isThread.mockReturnValueOnce(true);
-    ((mockInteraction.channel as GuildTextBasedChannel).members as DeepMockProxy<ThreadMemberManager>).fetch.mockResolvedValueOnce(mockMembers);
-    mockInteraction.options.getRole.mockReturnValueOnce({
+    interaction.channel?.isTextBased.mockReturnValueOnce(true);
+    interaction.channel?.isThread.mockReturnValueOnce(true);
+    ((interaction.channel as GuildTextBasedChannel).members as DeepMockProxy<ThreadMemberManager>).fetch.mockResolvedValueOnce(mockMembers);
+    interaction.options.getRole.mockReturnValueOnce({
       id: 'role-id',
       name: 'name',
     } as APIRole);
 
-    await removeUserByRole(mockInteraction);
+    await removeUserByRole(interaction);
 
-    expect(mockInteraction.deferReply).toHaveBeenCalled();
-    expect(mockInteraction.editReply).toHaveBeenCalled();
+    expect(interaction.deferReply).toHaveBeenCalled();
+    expect(interaction.editReply).toHaveBeenCalled();
   });
 });
