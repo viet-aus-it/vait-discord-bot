a98ccc37ece2c982d6db5ba58107c33a57d3934e
Sam Huynh <samhwang2112.dev@gmail.com>
2025-07-22 18:04:11 +1000

    test new prisma client

--- src/clients/db.ts
+++ src/clients/db.ts
@@ -1,9 +1,8 @@
-import { PrismaPg } from '@prisma/adapter-pg';
+// import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from './prisma/generated/client';
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+// const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({
-  adapter,
   omit: {
     serverChannelsSettings: {
       aocKey: true,
