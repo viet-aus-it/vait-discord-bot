d0a83a5c70863294c7deb84896ca4d886990ad3b
Sam Huynh <samhwang2112.dev@gmail.com>
2025-10-01 01:02:11 +1000

    feat: migrate prisma client

--- src/clients/db.ts
+++ src/clients/db.ts
@@ -1,14 +1,15 @@
-import { PrismaClient } from './prisma/generated/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from './prisma/generated/client/client';
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
 
 const prisma = new PrismaClient({
+  adapter,
   omit: {
     serverChannelsSettings: {
       aocKey: true,
-    },
-  },
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
     },
   },
 });
