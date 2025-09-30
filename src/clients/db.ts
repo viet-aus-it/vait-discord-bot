1a74fb36fb81f3652d66a9f2764d42aff19d9d01
Sam Huynh <samhwang2112.dev@gmail.com>
2025-10-01 01:18:19 +1000

    feat: load env while loading db module

--- src/clients/db.ts
+++ src/clients/db.ts
@@ -1,5 +1,8 @@
 import { PrismaPg } from '@prisma/adapter-pg';
+import { loadEnv } from 'src/utils/load-env';
 import { PrismaClient } from './prisma/generated/client/client';
+
+loadEnv();
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
