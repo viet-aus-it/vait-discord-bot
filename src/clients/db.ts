f71b763e8e327635989411a8f69dfb9c2990097d
Sam Huynh <samhwang2112.dev@gmail.com>
2025-10-02 09:09:19 +1000

    chore: update import path

--- src/clients/db.ts
+++ src/clients/db.ts
@@ -1,5 +1,5 @@
 import { PrismaPg } from '@prisma/adapter-pg';
-import { loadEnv } from 'src/utils/load-env';
+import { loadEnv } from '../utils/load-env';
 import { PrismaClient } from './prisma/generated/client/client';
 
 loadEnv();
