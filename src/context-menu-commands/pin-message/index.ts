5ca36992f30e52361412767afabe2e6d6aae3c91
Sam Huynh <samhwang2112.dev@gmail.com>
2025-12-08 10:29:24 +1100

    feat: make pin context menu command also unpin (#281)

--- src/context-menu-commands/pin-message/index.ts
+++ src/context-menu-commands/pin-message/index.ts
@@ -3,20 +3,28 @@ import { Result } from 'oxide.ts';
 import { logger } from '../../utils/logger';
 import type { ContextMenuCommand } from '../builder';
 
-export const data = new ContextMenuCommandBuilder().setName('Pin').setType(ApplicationCommandType.Message).setContexts(InteractionContextType.Guild);
+export const data = new ContextMenuCommandBuilder().setName('Pin/Unpin').setType(ApplicationCommandType.Message).setContexts(InteractionContextType.Guild);
 
-export const pinMessage = async (interaction: ContextMenuCommandInteraction) => {
+export const pinMessageCommand = async (interaction: ContextMenuCommandInteraction) => {
   if (!interaction.isMessageContextMenuCommand()) return;
 
   const message = interaction.targetMessage;
-  logger.info(`[pin]: Pinning message ${message.id} in channel ${message.channelId}`);
 
   if (message.pinned) {
-    logger.info(`[pin]: Message ${message.id} is already pinned.`);
-    await interaction.reply('Message is already pinned. Skipping...');
+    logger.info(`[pin]: Message ${message.id} is already pinned. Unpinning...`);
+    const op = await Result.safe(message.unpin());
+    if (op.isErr()) {
+      console.log(op.unwrapErr());
+      logger.error(`[pin]: Cannot unpin message ${message.id} in channel ${message.channelId}`, op.unwrapErr());
+      await interaction.reply('ERROR: Cannot unpin message. Please try again later.');
+      return;
+    }
+    logger.info(`[pin]: Successfully unpinned message ${message.id} in channel ${message.channelId}`);
+    await interaction.reply('Message is now unpinned!');
     return;
   }
 
+  logger.info(`[pin]: Message ${message.id} is not pinned. Pinning...`);
   const op = await Result.safe(message.pin());
   if (op.isErr()) {
     logger.error(`[pin]: Cannot pin message ${message.id} in channel ${message.channelId}`, op.unwrapErr());
@@ -30,7 +38,7 @@ export const pinMessage = async (interaction: ContextMenuCommandInteraction) => 
 
 const command: ContextMenuCommand = {
   data,
-  execute: pinMessage,
+  execute: pinMessageCommand,
 };
 
 export default command;
