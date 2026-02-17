2f2c1d3c423be196e3a01a77c6ac1afa1d4b9449
Sam Huynh <samhwang2112.dev@gmail.com>
2026-02-17 23:08:12 +1100

    feat: add batch span processor for prod

--- bin/telemetry.ts
+++ bin/telemetry.ts
@@ -2,15 +2,15 @@ import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentation
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { resourceFromAttributes } from '@opentelemetry/resources';
 import { NodeSDK } from '@opentelemetry/sdk-node';
+import { BatchSpanProcessor, SimpleSpanProcessor, type SpanProcessor } from '@opentelemetry/sdk-trace-base';
 import { ATTR_SERVICE_NAME, ATTR_SERVICE_VERSION } from '@opentelemetry/semantic-conventions';
 import { loadEnv } from '../src/utils/load-env';
 
 loadEnv();
 
 const serviceName = process.env.OTEL_SERVICE_NAME ?? 'vait-discord-bot';
-
 const otelEndpoint = process.env.OTEL_EXPORTER_OTLP_ENDPOINT;
-console.log({ otelEndpoint });
+
 const resource = resourceFromAttributes({
   [ATTR_SERVICE_NAME]: serviceName,
   [ATTR_SERVICE_VERSION]: '1.0.0',
@@ -18,15 +18,35 @@ const resource = resourceFromAttributes({
 
 const instrumentations = getNodeAutoInstrumentations();
 
+function getSpanProcessor(): SpanProcessor {
+  const localTraceExporter = new OTLPTraceExporter({ url: otelEndpoint });
+  const localSpanProcessor = new SimpleSpanProcessor(localTraceExporter);
+
+  const productionTraceExporter = new OTLPTraceExporter({
+    url: otelEndpoint,
+    headers: {
+      Authorization: process.env.AXIOM_TOKEN || '',
+      'X-Axiom-Dataset': process.env.AXIOM_DATASET || '',
+    },
+  });
+  const productionSpanProcessor = new BatchSpanProcessor(productionTraceExporter);
+
+  return process.env.NODE_ENV === 'production' ? productionSpanProcessor : localSpanProcessor;
+}
+const spanProcessor = getSpanProcessor();
+
 const sdk = new NodeSDK({
   resource,
   instrumentations: [instrumentations],
-  traceExporter: new OTLPTraceExporter({ url: otelEndpoint }),
+  spanProcessors: [getSpanProcessor()],
 });
 
 sdk.start();
 
 process.on('SIGTERM', () => {
+  spanProcessor.forceFlush();
+  spanProcessor.shutdown();
+
   sdk
     .shutdown()
     .then(() => console.log('Telemetry SDK shut down gracefully'))
