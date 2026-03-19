b1d28e2f8e9c934c7cb8797c74d66ab7d7c332ff
Sam Huynh <samhwang2112.dev@gmail.com>
2026-03-20 10:47:09 +1100

    feat: register honeypot-channel in server-settings command

--- src/slash-commands/server-settings/index.ts
+++ src/slash-commands/server-settings/index.ts
@@ -1,17 +1,19 @@
 import { InteractionContextType, PermissionFlagsBits, SlashCommandBuilder } from 'discord.js';
 import type { SlashCommand, SlashCommandHandler } from '../builder';
 import setAocSettings from './set-aoc-settings';
+import setHoneypotChannel from './set-honeypot-channel';
 import setReminderChannel from './set-reminder-channel';
 
 const data = new SlashCommandBuilder()
   .setName('server-settings')
   .setDescription('ADMIN ONLY COMMAND. Server Settings.')
   .addSubcommand(setReminderChannel.data)
+  .addSubcommand(setHoneypotChannel.data)
   .addSubcommand(setAocSettings.data)
   .setContexts(InteractionContextType.Guild)
   .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels);
 
-const subcommands = [setReminderChannel, setAocSettings];
+const subcommands = [setReminderChannel, setHoneypotChannel, setAocSettings];
 
 const execute: SlashCommandHandler = async (interaction) => {
   const requestedSubcommand = interaction.options.getSubcommand(true);
