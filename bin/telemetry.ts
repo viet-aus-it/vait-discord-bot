fe2a1cef43d605e5722470579446bf9b77af0e37
Sam Huynh <samhwang2112.dev@gmail.com>
2026-03-25 11:55:38 +1100

    feat: use batch exporter for prod and simple for local

--- bin/telemetry.ts
+++ bin/telemetry.ts
@@ -2,6 +2,7 @@ import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentation
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { resourceFromAttributes } from '@opentelemetry/resources';
 import { NodeSDK } from '@opentelemetry/sdk-node';
+import { BatchSpanProcessor, SimpleSpanProcessor, type SpanProcessor } from '@opentelemetry/sdk-trace-base';
 import { ATTR_SERVICE_NAME, ATTR_SERVICE_VERSION } from '@opentelemetry/semantic-conventions';
 import { loadEnv } from '../src/utils/load-env';
 
@@ -9,7 +10,6 @@ loadEnv();
 
 const serviceName = process.env.OTEL_SERVICE_NAME ?? 'vait-discord-bot';
 const otelEndpoint = process.env.OTEL_EXPORTER_OTLP_ENDPOINT;
-
 const resource = resourceFromAttributes({
   [ATTR_SERVICE_NAME]: serviceName,
   [ATTR_SERVICE_VERSION]: '1.0.0',
@@ -20,7 +20,7 @@ const instrumentations = getNodeAutoInstrumentations();
 function getTraceExporter(): OTLPTraceExporter {
   const localTraceExporter = new OTLPTraceExporter({
     url: otelEndpoint,
-    headers: process.env.OPENOBSERVE_AUTH_TOKEN ? { Authorization: process.env.OPENOBSERVE_AUTH_TOKEN } : {},
+    headers: { Authorization: process.env.OPENOBSERVE_AUTH_TOKEN ?? '' },
   });
 
   const productionTraceExporter = new OTLPTraceExporter({
@@ -35,10 +35,17 @@ function getTraceExporter(): OTLPTraceExporter {
 }
 const traceExporter = getTraceExporter();
 
+function getSpanProcessor(exporter: OTLPTraceExporter): SpanProcessor {
+  const localSpanProcessor = new SimpleSpanProcessor(exporter);
+  const productionSpanProcessor = new BatchSpanProcessor(exporter);
+  return process.env.NODE_ENV === 'production' ? productionSpanProcessor : localSpanProcessor;
+}
+const spanProcessor = getSpanProcessor(traceExporter);
+
 const sdk = new NodeSDK({
   resource,
   instrumentations: [instrumentations],
-  traceExporter,
+  spanProcessors: [spanProcessor],
   metricReaders: [],
   logRecordProcessors: [],
 });
