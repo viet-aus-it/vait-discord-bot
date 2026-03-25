8f719cb7eeb3277a1059f87e043ce19a5a8b340a
Sam Huynh <samhwang2112.dev@gmail.com>
2026-03-25 13:16:01 +1100

    feat: add spans to reputation DB utils

--- src/slash-commands/reputation/utils.ts
+++ src/slash-commands/reputation/utils.ts
@@ -1,14 +1,22 @@
 import { getDbClient } from '../../clients';
+import { tracer } from '../../utils/tracer';
 
 export const getOrCreateUser = async (userId: string) => {
-  const db = getDbClient();
+  return tracer.startActiveSpan('db.user.getOrCreate', async (span) => {
+    try {
+      span.setAttribute('user.id', userId);
+      const db = getDbClient();
 
-  let user = await db.user.findUnique({ where: { id: userId } });
-  if (!user) {
-    user = await db.user.create({ data: { id: userId } });
-  }
+      let user = await db.user.findUnique({ where: { id: userId } });
+      if (!user) {
+        user = await db.user.create({ data: { id: userId } });
+      }
 
-  return user;
+      return user;
+    } finally {
+      span.end();
+    }
+  });
 };
 
 type NumberAdjustment = { increment: number } | { decrement: number } | { set: number };
@@ -25,38 +33,52 @@ interface IUpdateRep {
 }
 
 export const updateRep = async ({ fromUserId, toUserId, adjustment }: IUpdateRep) => {
-  const db = getDbClient();
+  return tracer.startActiveSpan('db.reputation.update', async (span) => {
+    try {
+      span.setAttribute('fromUserId', fromUserId);
+      span.setAttribute('toUserId', toUserId);
+      const db = getDbClient();
 
-  const updatedUserPromise = db.user.update({
-    where: { id: toUserId },
-    data: adjustment,
+      const updatedUserPromise = db.user.update({
+        where: { id: toUserId },
+        data: adjustment,
+      });
+
+      const operation = getAdjustmentOperation(adjustment.reputation);
+
+      const logPromise = db.reputationLog.create({
+        data: {
+          fromUserId,
+          toUserId,
+          operation,
+        },
+      });
+
+      const [updatedUser] = await db.$transaction([updatedUserPromise, logPromise]);
+
+      return updatedUser;
+    } finally {
+      span.end();
+    }
   });
-
-  const operation = getAdjustmentOperation(adjustment.reputation);
-
-  const logPromise = db.reputationLog.create({
-    data: {
-      fromUserId,
-      toUserId,
-      operation,
-    },
-  });
-
-  const [updatedUser] = await db.$transaction([updatedUserPromise, logPromise]);
-
-  return updatedUser;
 };
 
 export const getRepLeaderboard = async (size: number) => {
-  const db = getDbClient();
+  return tracer.startActiveSpan('db.reputation.leaderboard', async (span) => {
+    try {
+      const db = getDbClient();
 
-  return db.user.findMany({
-    where: { reputation: { gt: 0 } },
-    orderBy: [{ reputation: 'desc' }],
-    select: {
-      id: true,
-      reputation: true,
-    },
-    take: size,
+      return db.user.findMany({
+        where: { reputation: { gt: 0 } },
+        orderBy: [{ reputation: 'desc' }],
+        select: {
+          id: true,
+          reputation: true,
+        },
+        take: size,
+      });
+    } finally {
+      span.end();
+    }
   });
 };
