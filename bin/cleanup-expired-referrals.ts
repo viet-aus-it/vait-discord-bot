753919b7cc88188794b1903fa98d88d033bed99c
Sam Huynh <samhwang2112.dev@gmail.com>
2026-03-30 23:43:13 +1100

    refactor: replace try/finally with Result.safe in bin/cleanup-expired-referrals.ts

--- bin/cleanup-expired-referrals.ts
+++ bin/cleanup-expired-referrals.ts
@@ -4,27 +4,30 @@ import { loadEnv } from '../src/utils/load-env';
 import { logger } from '../src/utils/logger';
 import { recordSpanError, tracer } from '../src/utils/tracer';
 
+const handleCleanup = async () => {
+  const op = await Result.safe(cleanupExpiredCode());
+  if (op.isErr()) {
+    recordSpanError(op.unwrapErr(), 'err-cleanup-referrals-failed');
+    logger.error('[cleanup-expired-referrals]: Error cleaning up expired referrals', op.unwrapErr());
+    return 1;
+  }
+
+  logger.info('[cleanup-expired-referrals]: Removed expired referrals');
+  return 0;
+};
+
 const cleanup = async () => {
   loadEnv();
   logger.info('[cleanup-expired-referrals]: CLEANING UP EXPIRED REFERRALS');
 
-  return tracer.startActiveSpan('cleanupExpiredReferrals', async (span) => {
-    try {
-      const op = await Result.safe(cleanupExpiredCode());
-      if (op.isErr()) {
-        recordSpanError(op.unwrapErr(), 'err-cleanup-referrals-failed');
-        logger.error('[cleanup-expired-referrals]: Error cleaning up expired referrals', op.unwrapErr());
-        span.end();
-        process.exit(1);
-      }
-
-      logger.info('[cleanup-expired-referrals]: Removed expired referrals');
-      span.end();
-      process.exit(0);
-    } finally {
-      span.end();
-    }
+  const result = await tracer.startActiveSpan('cleanupExpiredReferrals', async (span) => {
+    const op = await Result.safe(handleCleanup());
+    span.end();
+    return op;
   });
+
+  const exitCode = result.isOk() ? result.unwrap() : 1;
+  process.exit(exitCode);
 };
 
 cleanup();
