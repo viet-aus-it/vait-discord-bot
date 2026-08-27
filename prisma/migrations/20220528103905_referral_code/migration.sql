146dce2006d323332906863ebb425bcee92c3e88
Sam Huynh <samhwang2112.dev@gmail.com>
2026-08-27 20:54:11 +1000

    chore: uninstall prisma and remove prisma config files
    Remove prisma deps, schema, migrations, db-migrate compose service, DIRECT_DATABASE_URL env vars, and prisma buildScripts.

--- prisma/migrations/20220528103905_referral_code/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "ReferralCode" (
-    "id" TEXT NOT NULL,
-    "service" TEXT NOT NULL,
-    "code" TEXT NOT NULL,
-    "expiry_date" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "ReferralCode_pkey" PRIMARY KEY ("id")
-);
--- prisma/migrations/20230517115600_autobump_thread_server_setting/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "ServerChannelsSettings" ADD COLUMN     "autobumpThreads" TEXT[] DEFAULT ARRAY[]::TEXT[];
